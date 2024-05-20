3833c145710062dd53c77ab760c639dfaf4d9845
QuiMatematica <claudio@quimatematica.it>
2024-05-20 18:12:38 +0200

    Struttura a tre livelli

--- tao/othello.js
+++ tao/othello.js
@@ -11,6 +11,8 @@ import Score from './score.js'
 const boards = []
 
 export function init() {
+    loadIndex();
+
     document.querySelectorAll('.free-game-board').forEach((item) => {
         const freeGame = new FreeGameBoard(item, boards.length);
         boards.push(freeGame);
@@ -670,14 +672,6 @@ function matchOnEndClick(event) {
     }
 }
 
-function loadIndex() {
-    const sectionIndex = "../index.json#20240410";
-    let json;
-    fetch(sectionIndex)
-        .then((response) => response.json())
-        .then((json) => initPage(json));
-}
-
 function initHeader() {
     const brand = document.createElement("a");
     brand.classList.add("navbar-brand")
@@ -700,7 +694,7 @@ function initHeader() {
 
 }
 
-function addPage(page, ul) {
+function addPage(section, chapter, page, ul, inChapter) {
     const li = document.createElement("li");
     li.classList.add("nav-item");
     ul.append(li);
@@ -710,12 +704,22 @@ function addPage(page, ul) {
     a.classList.add("link-offset-2");
     a.classList.add("link-underline-opacity-0");
     a.classList.add("link-underline-opacity-100-hover");
-    a.setAttribute("href", "../" + page.href);
+
+    let pageHref;
+    if (inChapter) {
+        pageHref = "../../";
+    }
+    else {
+        pageHref = "../";
+    }
+    pageHref += section.href + chapter.href + page.href;
+
+    a.setAttribute("href", pageHref);
     a.innerHTML = page.title;
     li.append(a);
 }
 
-function addSection(section, ul) {
+function addChapter(section, chapter, ul, inChapter) {
     const li = document.createElement("li");
     li.classList.add("nav-item");
     li.classList.add("pb-3");
@@ -726,17 +730,61 @@ function addSection(section, ul) {
     a.classList.add("link-offset-2");
     a.classList.add("link-underline-opacity-0");
     a.classList.add("link-underline-opacity-100-hover");
-    a.setAttribute("href", "../" + section.href);
-    a.innerHTML = section.title;
+
+    let chapterHref;
+    if (inChapter) {
+        chapterHref = "../../";
+    }
+    else {
+        chapterHref = "../";
+    }
+    chapterHref += section.href + chapter.href + "chapter.html";
+
+    a.setAttribute("href", chapterHref);
+    a.innerHTML = chapter.title;
     li.append(a);
 
     const ul2 = document.createElement("ul");
     li.append(ul2);
 
-    section.pages.forEach((page) => addPage(page, ul2));
+    chapter.pages.forEach((page) => addPage(section, chapter, page, ul2, inChapter));
 }
 
-function initOffcanvas(json) {
+function addSection(section, offcanvasBody, inChapter) {
+    const h5 = document.createElement("h5");
+    offcanvasBody.append(h5);
+
+    const a = document.createElement("a");
+    a.classList.add("link-dark");
+    a.classList.add("link-offset-2");
+    a.classList.add("link-underline-opacity-0");
+    a.classList.add("link-underline-opacity-100-hover");
+
+    let sectionHref;
+    if (inChapter) {
+        sectionHref = "../../";
+    }
+    else {
+        sectionHref = "../";
+    }
+    sectionHref += section.href + "section.html";
+
+    a.setAttribute("href", sectionHref);
+    a.innerHTML = section.title;
+    h5.append(a);
+
+
+    const ul = document.createElement("ul");
+    ul.classList.add("nav");
+    ul.classList.add("flex-column");
+    offcanvasBody.append(ul);
+
+    section.chapters.forEach((chapter) => addChapter(section, chapter, ul, inChapter))
+
+    offcanvasBody.append(document.createElement("hr"))
+}
+
+function initOffcanvas(json, inChapter) {
     const offcanvas = document.createElement("div");
     offcanvas.classList.add("offcanvas");
     offcanvas.classList.add("offcanvas-start");
@@ -767,12 +815,7 @@ function initOffcanvas(json) {
     offcanvasBody.classList.add("offcanvas-body");
     offcanvas.append(offcanvasBody);
 
-    const ul = document.createElement("ul");
-    ul.classList.add("nav");
-    ul.classList.add("flex-column");
-    offcanvasBody.append(ul);
-
-    json.sections.forEach((section) => addSection(section, ul))
+    json.sections.forEach((section) => addSection(section, offcanvasBody, inChapter))
 }
 
 function buildPreviousNext(previous, symbol, page) {
@@ -842,41 +885,96 @@ function buildPagination(json, prevPage, nextPage) {
     return nav;
 }
 
+function loadIndex() {
+    const urlSplitted = window.location.pathname.split("/")
+    const fileName = urlSplitted.pop();
+    let indexHref;
+    if (fileName == "section.html") {
+        indexHref = "../index.json#20240410";
+    }
+    else {
+        indexHref = "../../index.json#20240410";
+    }
+
+    let json;
+    fetch(indexHref)
+        .then((response) => response.json())
+        .then((json) => initPage(json));
+}
+
 function initPage(json) {
     const urlSplitted = window.location.pathname.split("/")
     const fileName = urlSplitted.pop();
-    const sectionName = urlSplitted.pop();
-    const sectionIndex = json.sections.findIndex(x => x.href == sectionName + "/index.html");
+    let sectionName;
+    let chapterName;
+    let inChapter = false;
+    if (fileName == "section.html") {
+        sectionName = urlSplitted.pop();
+    }
+    else {
+        chapterName = urlSplitted.pop();
+        sectionName = urlSplitted.pop();
+        inChapter = true;
+    }
+
+    const sectionIndex = json.sections.findIndex(x => x.href == sectionName + "/");
     const section = json.sections[sectionIndex];
-    const pageIndex = section.pages.findIndex(x => x.href == sectionName + "/" + fileName);
-    // se non lo trova => pageIndex == -1
+
+    let chapterIndex = -1;
+    let chapter;
+    let pageIndex = -1;
+    if (chapterName != null) {
+        chapterIndex = section.chapters.findIndex(x => x.href == chapterName + "/");
+        chapter = section.chapters[chapterIndex];
+
+        pageIndex = chapter.pages.findIndex(x => x.href == fileName);
+        // se non lo trova => pageIndex == -1
+    }
+
+    initOffcanvas(json, inChapter);
+    initHeader();
+
     var prevPage = null;
     var nextPage = null;
     if (pageIndex > 0) {
-        prevPage = section.pages[pageIndex - 1];
+        prevPage = chapter.pages[pageIndex - 1];
     }
     else if (pageIndex == 0) {
-        prevPage = section;
+        // sei alla prima pagina di un capitolo
+        if (chapterIndex > 0) {
+            prevChapter = section.chapters[chapterIndex - 1];
+            prevPage = prevChapter.pages.slice(-1);
+        }
+        else {
+            prevPage = section
+        }
     }
     else {
         if (sectionIndex > 0) {
+            // sei nell'introduzione di una sezione, e non è la prima sezione
             const prevSection = json.sections[sectionIndex - 1];
-            prevPage = prevSection.pages[prevSection.pages.length - 1];
+            const prevChapter = prevSection.chapters.slice(-1);
+            prevPage = prevSection.pages.slice(-1);
         }
+        // altrimenti sei nell'introduzione della prima sezione o in una pagina ignota all'indice
     }
-    if (pageIndex < section.pages.length - 1) {
-        nextPage = section.pages[pageIndex + 1];
+
+    if (chapter != null) {
+        if (pageIndex < chapter.pages.length - 1) {
+            nextPage = chapter.pages[pageIndex + 1];
+        }
+        else {
+            if (chapterIndex < section.chapters.length - 1) {
+                nextPage = section.chapters[chapterIndex + 1].pages[0];
+            }
+        }
     }
     else {
-        if (sectionIndex < json.sections.length - 1) {
-            nextPage = json.sections[sectionIndex + 1];
-        }
+        // sei all'inizio di una sezione
+        nextPage = section.chapters[0].pages[0]
     }
 
-    initOffcanvas(json);
-    initHeader();
-
     const othelloContent = document.getElementById("othello-content");
-    othelloContent.prepend(buildPagination(json, prevPage, nextPage));
-    othelloContent.append(buildPagination(json, prevPage, nextPage));
+    othelloContent.prepend(buildPagination(json, prevPage, nextPage, inChapter));
+    othelloContent.append(buildPagination(json, prevPage, nextPage, inChapter));
 }

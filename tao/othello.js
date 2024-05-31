a3f77b92f525bfc15ab5cecf9e56c24ff22c806c
QuiMatematica <claudio@quimatematica.it>
2024-05-31 09:58:20 +0200

    Rilascio del 31 maggio 2024

--- tao/othello.js
+++ tao/othello.js
@@ -895,10 +895,10 @@ function loadIndex() {
     const fileName = urlSplitted.pop();
     let indexHref;
     if (fileName == "section.html") {
-        indexHref = "../index.json#20240410";
+        indexHref = "../index.json#20240531";
     }
     else {
-        indexHref = "../../index.json#20240410";
+        indexHref = "../../index.json#20240531";
     }
 
     let json;

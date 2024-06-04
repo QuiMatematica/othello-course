ba308f34fe621d46b926b21f72a3d573962972b8
QuiMatematica <claudio@quimatematica.it>
2024-06-04 18:01:46 +0200

    Navigator

--- base/section.php
+++ base/section.php
@@ -17,9 +17,13 @@
     include '../classes/Navigator.php';
 
     $navigator = new Navigator();
+
+    $navigator->initHeader();
     ?>
 
 	<div id="othello-content" class="container-xxl mt-4">
+
+        <?php $navigator->pagination() ?>
 
         <h1>Livello base</h1>
 
@@ -29,6 +33,8 @@
             <li><a href="quiz-per-cominciare/chapter.html">Piccoli quiz per cominciare</a>: per allenare l'occhio a individuare tutte le mosse a disposizione e i loro effetti;</li>
             <li><a href="primi-elementi-di-strategia/chapter.html">Primi elementi di strategia</a>: per cominciare a orientarsi nella scelta delle mosse e vincere le tue prime partite.</li>
         </ul>
+
+        <?php $navigator->pagination() ?>
 
     </div>
 
--- classes/Navigator.php
+++ classes/Navigator.php
@@ -2,36 +2,115 @@
 
 class Navigator {
 
+    public $inChapter;
     public $section;
     public $chapter;
     public $page;
+    public $prevPage;
+    public $nextPage;
 
     public function __construct() {
         $uri = $_SERVER['REQUEST_URI'];
         $path = parse_url($uri, PHP_URL_PATH);
         $pathLevels = explode('/', trim($path, '/'));
 
-//            let inChapter = false;
-//    if (fileName == "section.html") {
-//        sectionName = urlSplitted.pop();
-//    }
-//    else {
-//        chapterName = urlSplitted.pop();
-//        sectionName = urlSplitted.pop();
-//        inChapter = true;
-//    }
+        $pageName = $pathLevels[count($pathLevels) - 1];
+        $sectionName = null;
+        $chapterName = null;
 
-        $fileName = $pathLevels[count($pathLevels) - 1];
-        if ($fileName === 'section.php') {
+        if ($pageName === 'section.php') {
             $indexHref = '../index.json';
+            $sectionName = $pathLevels[count($pathLevels) - 2];
+            $this->inChapter = false;
         }
         else {
             $indexHref = '../../index.json';
+            $chapterName = $pathLevels[count($pathLevels) - 2];
+            $sectionName = $pathLevels[count($pathLevels) - 3];
+            $this->inChapter = true;
         }
 
         $file = file_get_contents($indexHref);
         $json = json_decode($file, true);
 
+        $sectionIndex = array_search($sectionName . '/', array_column($json['sections'], 'href'));
+        $this->section = $json['sections'][$sectionIndex];
+
+        $chapterIndex = -1;
+        $pageIndex = -1;
+
+        if ($this->inChapter) {
+            $chapterIndex = array_search($chapterName . '/', array_column($this->section['chapters'], 'href'));
+            $this->chapter = $this->section['chapters'][$chapterIndex];
+
+            $pageIndex = array_search($pageName, array_column($this->chapter['pages'], 'href'));
+            $this->page = $this->chapter['pages'][$pageIndex];
+        }
+
+        if ($pageIndex > 0) {
+            $this->prevPage = $this->chapter['pages'][$pageIndex - 1];
+        }
+        else if ($pageIndex == 0) {
+            // sei alla prima pagina di un capitolo
+            $this->prevPage = $this->chapter;
+            $this->chapter['href'] = 'chapter.php';
+        }
+        else {
+            if ($chapterIndex > 0) {
+                // sei nell'introduzione di un capitolo, e non è il primo capitolo della sezione
+                $prevChapter = $this->section['chapters'][$chapterIndex - 1];
+                $this->prevPage = $prevChapter['pages'][count($prevChapter['pages']) - 1];
+            }
+        }
+
+        if ($this->chapter != null) {
+
+        }
+        else {
+            // sei all'inizio di una sezione
+            $this->nextPage = $this->section['chapters'][0];
+            $this->nextPage['href'] = 'chapter.php';
+        }
+    }
+
+    function initHeader() {
+        $index_href = ($this->inChapter) ? '../../index.php' : '../index.php';
+        echo "<nav class='navbar navbar-expand-lg bg-primary' data-bs-theme='dark'>";
+        echo "<div class='container-xxl'>";
+        echo "<a class='navbar-brand h1' href='{$index_href}'>Othello: corso interattivo</a>";
+        echo "</div>";
+        echo "</nav>";
+    }
+
+    function pagination() {
+        echo "<nav class='my-3'>";
+        echo "<ul class='pagination justify-content-center'>";
+        if ($this->prevPage != null) {
+            $this->buildPreviousNext(true);
+        }
+//        $this->buildOffcanvasButton();
+        if ($this->nextPage != null) {
+            $this->buildPreviousNext(false);
+        }
+        echo "</ul>";
+        echo "</nav>";
+    }
+
+    private function buildPreviousNext($prev) {
+        echo "<li class='page-item'>";
+        if ($prev) {
+            echo "<a class='page-link' aria-label='Previous' href='{$this->prevPage['href']}'>";
+            echo "<span class='px-1' aria-hidden='true'>&laquo;</span>";
+            echo "<span class='sr-only'>{$this->prevPage['title']}</span>";
+            echo "</a>";
+        }
+        else {
+            echo "<a class='page-link' aria-label='Previous' href='{$this->nextPage['href']}'>";
+            echo "<span class='sr-only'>{$this->nextPage['title']}</span>";
+            echo "<span class='px-1' aria-hidden='true'>&raquo;</span>";
+            echo "</a>";
+        }
+        echo "</li>";
     }
 
 }

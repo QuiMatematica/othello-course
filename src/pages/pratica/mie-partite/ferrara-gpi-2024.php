abb5d54f47a94ed45c6ea72677c48a72cb77f3f2
QuiMatematica <claudio@quimatematica.it>
2024-06-16 21:06:08 +0200

    Ferrara GPI 2024

---
+++ src/pages/pratica/mie-partite/ferrara-gpi-2024.php
@@ -0,0 +1,32 @@
+        <h1>Ferrara - GPI 2024</h1>
+
+        <h2>Signorini Claudio - Luzi Ilaria</h2>
+
+        <p>Average score lost: 0.87 (1.80, 0.00, 0.00)</p>
+
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+            <div class="col">
+
+				<div class="card mx-auto board-card my-3">
+					<div class="card-body">
+						<div class="referee-board" data-file="signorini-luzi-ferrara-gpi-2024.json"></div>
+					</div>
+				</div>
+
+            </div>
+            <div class="col">
+
+                <div class="card mx-auto board-card my-3">
+					<div class="card-body">
+						<div class="free-game-board"></div>
+					</div>
+				</div>
+
+            </div>
+        </div>
+
+        <div class="card mx-auto board-card my-3">
+			<div class="card-body">
+				<div class="sequence-board" data-file="signorini-luzi-ferrara-gpi-2024-mossa-9.json"></div>
+			</div>
+		</div>

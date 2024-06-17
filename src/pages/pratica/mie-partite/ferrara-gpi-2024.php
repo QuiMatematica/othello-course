9bf1eaa2aa2f5528470e95249a7edf702396217e
QuiMatematica <claudio@quimatematica.it>
2024-06-17 18:08:58 +0200

    Ferrara GPI 2024

--- src/pages/pratica/mie-partite/ferrara-gpi-2024.php
+++ src/pages/pratica/mie-partite/ferrara-gpi-2024.php
@@ -30,3 +30,59 @@
 				<div class="sequence-board" data-file="signorini-luzi-ferrara-gpi-2024-mossa-9.json"></div>
 			</div>
 		</div>
+
+        <h2>Foschi Tommaso - Signorini Claudio</h2>
+
+        <p>Average score lost: 0.67 (0.60, 1.00, 0.46)</p>
+
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+            <div class="col">
+
+				<div class="card mx-auto board-card my-3">
+					<div class="card-body">
+						<div class="referee-board" data-file="foschitommaso-signorini-ferrara-gpi-2024.json"></div>
+					</div>
+				</div>
+
+            </div>
+            <div class="col">
+
+                <div class="card mx-auto board-card my-3">
+					<div class="card-body">
+						<div class="free-game-board"></div>
+					</div>
+				</div>
+
+            </div>
+        </div>
+
+        <h2>Signorini Claudio - Foschi Damiano</h2>
+
+        <p>Average score lost: 1.87 (1.00, 2.80, 1.80)</p>
+
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+            <div class="col">
+
+				<div class="card mx-auto board-card my-3">
+					<div class="card-body">
+						<div class="referee-board" data-file="signorini-foschidamiano-ferrara-gpi-2024.json"></div>
+					</div>
+				</div>
+
+            </div>
+            <div class="col">
+
+                <div class="card mx-auto board-card my-3">
+					<div class="card-body">
+						<div class="free-game-board"></div>
+					</div>
+				</div>
+
+            </div>
+        </div>
+
+        <div class="card mx-auto board-card my-3">
+			<div class="card-body">
+				<div class="sequence-board" data-file="signorini-foschidamiano-ferrara-gpi-2024-mossa-15.json"></div>
+			</div>
+		</div>

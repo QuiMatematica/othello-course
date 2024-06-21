83dffdcdbaace6a87911a18db9bd6ee55c05a535
QuiMatematica <claudio@quimatematica.it>
2024-06-21 17:39:08 +0200

    Rimozione header

--- src/web/presentazione-del-corso.php
+++ src/web/presentazione-del-corso.php
@@ -2,7 +2,17 @@
 
 <html lang="it">
 <head>
-    <?php include 'header.php' ?>
+    <?php
+        if ($_SERVER['HTTP_HOST'] == 'othello.quimatematica.it') {
+            include 'google-tag.php';
+        }
+    ?>
+
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Othello: corso interattivo</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>

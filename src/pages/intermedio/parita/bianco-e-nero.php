246f2b16c0bce976f6694e44d9c8c7e05ec7f47c
QuiMatematica <claudio@quimatematica.it>
2024-08-16 21:17:55 +0200

    Parità

---
+++ src/pages/intermedio/parita/bianco-e-nero.php
@@ -0,0 +1,39 @@
+<h1>Il bianco e il nero</h1>
+
+<p>Se durante una partita nessuno dei due giocatori è costretto a passare il turno, ci
+sarà sempre un numero pari di caselle libere quando deve giocare il nero, e un numero
+    dispari di caselle libere quando deve giocare il bianco.
+    In un certo senso, questo vuol dire che
+è il bianco a detenere la parità fin dall'inizio della partita.</p>
+
+<p>Per questo motivo, fino a qualche anno fa, si era convinti che il bianco avesse
+un leggero vantaggio rispetto al nero e che la partita perfetta portasse a una
+vittoria (seppur limitata) del bianco. Ma 60 mosse sono tante e può succedere di tutto.
+    I recenti studi con il computer hanno evidenziato che
+ci sono moltissime sequenze che portano al pareggio (ma di questo parleremo più
+avanti).</p>
+
+<p>Resta il fatto che, dal punto di vista della parità, è più facile giocare con il
+bianco.</p>
+
+<p>E il nero, allora?</p>
+
+<p>Banalmente, la prima cosa che si può dire è che il nero ha la possibilità di
+vincere se arriva in finale con un numero sufficientemente alto di pedine stabili,
+in modo da bilanciare lo svantaggio derivante dalla parità.
+Muovendo per primo può girare più pedine per primo, in modo da cercare di mantenerle
+fino in fondo. Ma non è una strategia facilmente attuabile e il vantaggio dell'ultima mossa
+    è dominante su quello della prima mossa.</p>
+
+<p>Più concretamente, come abbiamo visto in molti degli esempi precedenti,
+    il nero può cercare di costruire delle regioni dispari in cui ha la parità.
+Così facendo può giocare in modo da riempire tutti gli altri spazi, per poi costringere
+l'avversario a passare, ottenendo così la parità.</p>
+
+<p>Una seconda possibilità, più complessa e più rischiosa, è di porsi in una situazione in cui
+è costretto a passare, in modo da ottenere comunque la possibilità di fare l'ultima mossa.</p>
+
+<p>Una terza possibilità più interessante, ma difficilmente attuabile, consiste nel costringere
+il bianco a concedere altri vantaggi strategici per evitare di perdere la parità.</p>
+
+<p>Ma in verità c'è un unico modo per vincere con il nero: giocare meglio del bianco.</p>

c274bfb2bfe2e27b6d2db26a1fc6e61fcf18957d
QuiMatematica <claudio@quimatematica.it>
2024-10-02 10:43:28 +0200

    Bordi

--- src/pages/intermedio/bordi/chapter.php
+++ src/pages/intermedio/bordi/chapter.php
@@ -9,16 +9,16 @@
         <p>Il gioco sui bordi deve essere guidato principalmente da tre obiettivi:</p>
 
         <ul>
-          <li>conquistare dei <b>tempi di gioco</b> e non consentire all'avversario di farlo;</li>
+          <li>conquistare dei
+              <a href="../tempi-di-gioco/chapter.php" title="I tempi di gioco"><b>tempi di gioco</b></a>
+              e non consentire all'avversario di farlo;</li>
+          <li>preparare gli spazi adeguati per giocare la
+              <a href="../parita/chapter.php" title="La parità"><b>parità</b></a>;</li>
           <li>ottenere delle <b>formazioni non attaccabili</b> e costringere l'avversario a prendere delle
-          <b>formazioni attaccabili</b>;</li>
-          <li>preparare gli spazi adeguati per giocare la <b>parità</b>.</li>
+          <b>formazioni attaccabili</b>.</li>
         </ul>
 
-        <p>Del primo obiettivo, i tempi di gioco, si è già parlato nel
-            <a href="../tempi-di-gioco/chapter.php" title="I tempi di gioco">capitolo precedente</a>.</p>
-
-        <p>Il terzo obiettivo, la parità, verrà trattato nel prossimo capitolo.</p>
+        <p>Dei primi due obiettivi si è già parlato nei capitoli precedenti.</p>
 
         <p>In questo capitolo cominceremo ad approcciare il secondo obiettivo:
         dopo una prima presentazione delle diverse formazioni di bordo verranno mostrate

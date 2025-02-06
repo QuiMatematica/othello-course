b047df121572588bbff65f6a57cf3de541de08e7
QuiMatematica <claudio@quimatematica.it>
2025-02-06 21:15:32 +0100

    Share button on page.

--- GeneratePagesPlugin.js
+++ GeneratePagesPlugin.js
@@ -196,6 +196,7 @@ class GeneratePagesPlugin {
 	<title>${title}</title>
 	<link href="${prepend}css/bootstrap.min.css" rel="stylesheet">
 	<script src="${prepend}js/bootstrap.bundle.min.js"></script>
+	<link rel="stylesheet" href="${prepend}assets/bootstrap-icons/bootstrap-icons.min.css">
 	<link rel="stylesheet" href="${prepend}css/othello.css">
 	<script type="module" src="${prepend}js/tao.js"></script>
 </head>
@@ -214,6 +215,9 @@ class GeneratePagesPlugin {
         </div>
     </div>
 	<div id="othello-content" class="container-xxl mt-4">
+        <button id="shareBtn" class="btn btn-primary share-button">
+            <i class="bi bi-share-fill"></i>
+        </button>
 ${pagination}
 ${htmlContent}
 ${pagination}

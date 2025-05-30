2cd99f12835ea86d86f9b96d848e95a4a0a8fba2
QuiMatematica <claudio@quimatematica.it>
2025-05-30 19:15:15 +0200

    Stoner trap

---
+++ src/pages/avanzato/stoner-trap/altre-stoner-trap.php
@@ -0,0 +1,59 @@
+<h1>Altri esempi di Stoner trap</h1>
+
+<p>Il diagramma 1 riporta la situazione alla 38-esima mossa della semifinale del Campionato
+Italiano 1984, tra Paolo Ghirardato (bianco) e Piero Zama (nero) (esempio tratto dal libro
+di Brusca, Maccheroni, Puzzo). Il bianco è in vantaggio perché ha chiuso il nero in
+maniera pressoché completa. Ma prima di gettare la spugna Piero tenterà di rendere la
+vita difficile al suo avversario eseguendo una variante delle Stoner Trap.</p>
+
+<div class="card mx-auto board-card my-3">
+	<div class="card-body">
+		<div class="match-file-board" data-file="altre-stoner-trap-1.json"></div>
+	</div>
+	<div class="card-footer text-body-secondary text-center">
+		Diagramma 1: variante di Stoner Trap.
+	</div>
+</div>
+
+<p>Secondo la classifica di questo corso sarebbe una Stoner trap di tipo A, visto che il bianco dovrebbe giocare
+in <b>C1</b> per non cedere l'angolo. Ma la presenza della pedina <b>B1</b> la rende più simile, per quanto riguarda
+la parità, alle Stoner trap di tipo C.</p>
+
+<p>Il diagramma 2 riporta la posizione alla 36-esima mosse della finale del Campionato
+del Mondo 1983 tra il giapponese Ken Ichi Ishii (bianco) e l'inglese Imre Leader (nero)
+(anche questo esempio è tratto dal libro di Brusca, Maccheroni, Puzzo). La struttura a
+nord è un quattro sbilanciato <i>bucato</i>. Se il nero volesse chiudere il quattro avversario
+con <b>C1</b>, seguita dalla forzata <b>D1</b>, non sarebbe possibile giocare una Stoner trap. Ma è
+possibile attaccare la formazione anche se è bucata.</p>
+
+<div class="card mx-auto board-card my-3">
+	<div class="card-body">
+		<div class="match-file-board" data-file="altre-stoner-trap-2.json"></div>
+	</div>
+	<div class="card-footer text-body-secondary text-center">
+		Diagramma 2: Stoner trap a un quattro sbilanciato bucato.
+	</div>
+</div>
+
+<p>Il diagramma 3 presenta una partita giocata on-line da Paolo Tormene (bianco). La posizione è anomala rispetto
+a quelle già viste, ma è a tutti gli effetti una Stoner trap di tipo C.</p>
+
+<div class="card mx-auto board-card my-3">
+	<div class="card-body">
+		<div class="match-file-board" data-file="altre-stoner-trap-3.json"></div>
+	</div>
+	<div class="card-footer text-body-secondary text-center">
+		Diagramma 3.
+	</div>
+</div>
+
+<h2>Tocca a te</h2>
+
+<div class="card mx-auto board-card my-3">
+	<div class="card-body">
+		<div class="sequence-board" data-file="altre-stoner-trap-4.json"></div>
+	</div>
+	<div class="card-footer text-body-secondary text-center">
+		Diagramma 4: il bianco muove e conquista 11 pedine stabili in 4 mosse.
+	</div>
+</div>

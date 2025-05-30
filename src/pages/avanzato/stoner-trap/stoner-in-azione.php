708009cf07f5a01efb82fe2d8584306998a91383
QuiMatematica <claudio@quimatematica.it>
2025-05-30 20:39:08 +0200

    Stoner trap

---
+++ src/pages/avanzato/stoner-trap/stoner-in-azione.php
@@ -0,0 +1,46 @@
+<h1>Le Stoner trap in azione</h1>
+
+<p>Mi ha colpito un'affermazione del maestro Paolo Bianchi scritta sulla chat nazionale della F.N.G.O.:</p>
+
+<p class="text-center"><i>Belle le Stoner, ma anche molto rare a verificarsi.
+        Nelle centinaia di partite giocate ne ho subito pochissime, forse solo una, da Caspard
+        in un torneo milanese degli anni '90.</i></p>
+
+<p>Ma poi ho cercato di ricordarmi quante Stoner trap avessi giocato nelle mie partite.</p>
+
+<p>Me ne ricordo molto bene una. 22 aprile 1990: avevo 15 anni. Ero al mio secondo torneo, in quel di Modena,
+ospite dei simpaticissimi Angela Quattrocchi e Luca Ottani. Stavo giocando proprio contro Angela. E all'improvviso
+eccola lì: la possibilità di giocare la mia prima Stoner trap. E ho vinto la partita.</p>
+
+<p>Altre... no! Certo non ricordo a memoria tutte le mie partite, ma ho un ricordo di tutte le più significative.
+E non ricordo nessun'altra Stoner trap.</p>
+
+<p>Ma allora è vero che sono così rare? Al punto che forse non vale la pena studiarle?</p>
+
+<h2>Stoner trap: the phantom menace</h2>
+
+<p>Sì, la citazione non è casuale. <i>The phantom menace</i>: <i>la minaccia fantasma</i>, il titolo del primo episodio
+della saga di Guerre Stellari.</p>
+
+<p>Anche se non si vedono, le Stoner trap sono spesso dietro l'angolo. Sono una minaccia che può concretizzarsi
+    da un momento all'altro. Ma sono minacce da cui abbiamo imparato a difenderci. Della serie:
+    <i>se le conosci le eviti</i>. E se le eviti non le vedi.</p>
+
+<p>Ma state certi che se, ad alto livello, un giocatore ottiene una formazione sbilanciata, subito entrambi pensano
+alla Stoner trap: l'uno per capire se può utilizzarla, l'altro per fare di tutto per evitarla.</p>
+
+<p>Le condizioni per realizzare una Stoner trap non sono facili da ottenere, in particolare il controllo della diagonale.
+Quindi al giocatore minacciato basta assicurarsi di avere sempre una pedina sulla diagonale e l'attacco è sventato.
+Ma talvolta gli sforzi di mantenere una situazione di non attaccabilità possono costare cari, come la perdita di mobilità
+in una regione.</p>
+
+<p>Quindi tra due giocatori che conoscono bene il gioco, la Stoner trap può realizzarsi essenzialmente in due occasioni:</p>
+<ul>
+    <li>chi può essere attaccato non si è accorto che ci sono tutte le condizioni per subire l'attacco;</li>
+    <li>chi può essere attaccato ha valutato meno dannoso l'attacco delle manovre necessarie a pararlo.</li>
+</ul>
+
+<h2>Non se n'è accorto</h2>
+
+<p>Il diagramma 1 riporta la situazione dopo 29-esima mossa della partita tra l'inglese Imre Leader (nero) e
+il tedesco Matthias Berg (bianco) all'ottavo turno del torneo di Cambridge dell'EGP 2024-2025.</p>

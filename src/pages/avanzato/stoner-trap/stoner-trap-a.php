ce68782aa95600b513282ba6c4a609637a6f00d5
QuiMatematica <claudio@quimatematica.it>
2025-05-30 17:37:59 +0200

    Stoner trap

---
+++ src/pages/avanzato/stoner-trap/stoner-trap-a.php
@@ -0,0 +1,151 @@
+<h1>Le Stoner trap di tipo A</h1>
+
+<p>Le Stoner trap che chiamo <i>di tipo A</i> sono quelle che, alla fine
+dell'attacco, vedono <u>quattro pedine</u> sul bordo attaccato. Il colore che deve
+difendersi potrebbe conquistare tutto il lato giocando in una casella <b>A</b>, ma,
+    se l'attacco è condotto bene, questa mossa gira la casella <b>X</b> adiacente,
+consentendo all'attaccante di conquistare l'angolo.</p>
+
+<p>Nel diagramma 1 vediamo il caso più semplice e più comune di questo tipo di attacco.</p>
+
+<div class="card mx-auto board-card my-3">
+	<div class="card-body">
+		<div class="match-file-board" data-file="stoner-trap-a-1.json"></div>
+	</div>
+	<div class="card-footer text-body-secondary text-center">
+		Diagramma 1.
+	</div>
+</div>
+
+<h2>Condizioni per la riuscita dell'attacco</h2>
+
+<p>Deve esserci una formazione di bordo sbilanciata da attaccare. Nell'esempio è un tre (diagramma 2), ma vedremo che è
+possibile attaccare altre formazioni.</p>
+
+<div class="card mx-auto board-card my-3">
+	<div class="card-body">
+		<div class="match-file-board" data-file="stoner-trap-a-2.json"></div>
+	</div>
+	<div class="card-footer text-body-secondary text-center">
+		Diagramma 2.
+	</div>
+</div>
+
+<p>L'attaccante (il bianco) deve poter giocare nella casella <b>X</b> opposta allo schieramento (<b>B7</b>).
+Come vedremo dopo non è necessario prendere il controllo di tutta la diagonale, ma farlo rende l'attacco più
+efficace (diagramma 3).</p>
+
+<div class="card mx-auto board-card my-3">
+	<div class="card-body">
+		<div class="match-file-board" data-file="stoner-trap-a-3.json"></div>
+	</div>
+	<div class="card-footer text-body-secondary text-center">
+		Diagramma 3.
+	</div>
+</div>
+
+<p>Dopo la risposta dell'attaccato, l'attaccante (il bianco) deve poter giocare nella casella adiacente alla
+formazione sbilanciata (<b>D8</b>, diagramma 4).</p>
+
+<div class="card mx-auto board-card my-3">
+	<div class="card-body">
+		<div class="match-file-board" data-file="stoner-trap-a-4.json"></div>
+	</div>
+	<div class="card-footer text-body-secondary text-center">
+		Diagramma 4.
+	</div>
+</div>
+
+<p>Completato l'attacco, se l'attaccato (il nero) gioca nella casella <b>A</b> rimasta libera (<b>C8</b>),
+deve girare anche la casella <b>X</b> (<b>B7</b>) in modo che l'attaccante possa accedere all'angolo.</p>
+
+<div class="card mx-auto board-card my-3">
+	<div class="card-body">
+		<div class="match-file-board" data-file="stoner-trap-a-5.json"></div>
+	</div>
+	<div class="card-footer text-body-secondary text-center">
+		Diagramma 5.
+	</div>
+</div>
+
+<h2>Se non ci sono tutte le condizioni</h2>
+
+<p>Questo tipo di Stoner trap sono molto più facili da giocare perché non hanno
+alcuna condizione necessaria alla riuscita. Se tali condizioni vengono a mancare
+non si può più parlare propriamente di una Stoner trap, ma l'attacco ha comunque
+effetto. Per contro, le formazioni di bordo attaccabili con queste trappole
+sono molto più rare.</p>
+
+<p>Vediamo comunque ciascuna delle condizioni viste per quelle
+<a href="stoner-trap-c.php">di tipo C</a> e cerchiamo di capire
+perché non sono necessarie. </p>
+
+<p>Giocando nella casella <b>X</b> non è necessario ottenere il controllo della diagonale.
+    Infatti anche se l'attaccato conquista l'angolo, sul bordo rimangono tre caselle
+vuote e l'attaccante può incunearsi (diagramma 6).</p>
+
+<div class="card mx-auto board-card my-3">
+	<div class="card-body">
+		<div class="match-file-board" data-file="stoner-trap-a-6.json"></div>
+	</div>
+	<div class="card-footer text-body-secondary text-center">
+		Diagramma 6.
+	</div>
+</div>
+
+<p>Non è necessario che l'attaccante possa chiudere la trappola giocando sul bordo
+accanto alla formazione sbilanciata avversaria. Infatti anche se non ci riesce
+e l'attaccato prende l'angolo, rimane uno spazio dispari che permette l'incuneamento (diagramma 5).
+
+<div class="card mx-auto board-card my-3">
+	<div class="card-body">
+		<div class="match-file-board" data-file="stoner-trap-a-7.json"></div>
+	</div>
+	<div class="card-footer text-body-secondary text-center">
+		Diagramma 7.
+	</div>
+</div>
+
+<p>Non è nemmeno necessario che l'eventuale mossa dell'attaccato nella casella <b>A</b>
+    giri la pedina nella casella <b>X</b>. In questo caso l'attacco diventa un normale
+attacco a un cinque e come tale va gestito (diagramma 8).</p>
+
+<div class="card mx-auto board-card my-3">
+	<div class="card-body">
+		<div class="match-file-board" data-file="stoner-trap-a-8.json"></div>
+	</div>
+	<div class="card-footer text-body-secondary text-center">
+		Diagramma 8.
+	</div>
+</div>
+
+<h2>Altre formazioni sbilanciate attaccabili</h2>
+
+<p>Il tre sbilanciato è solo una delle formazioni di bordo attaccabili con una Stoner trap di tipo A.</p>
+
+<div class="row row-cols-1 row-cols-md-2 g-4">
+	<div class="col">
+
+		<div class="card mx-auto board-card my-3">
+			<div class="card-body">
+				<div class="match-file-board" data-file="stoner-trap-a-9.json"></div>
+			</div>
+			<div class="card-footer text-body-secondary text-center">
+				Diagramma 9.
+			</div>
+		</div>
+
+	</div>
+	<div class="col">
+
+		<div class="card mx-auto board-card my-3">
+			<div class="card-body">
+				<div class="match-file-board" data-file="stoner-trap-a-10.json"></div>
+			</div>
+			<div class="card-footer text-body-secondary text-center">
+				Diagramma 10.
+			</div>
+		</div>
+
+	</div>
+</div>

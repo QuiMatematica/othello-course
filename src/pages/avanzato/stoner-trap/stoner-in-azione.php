8c0550af6a12ade7c1e7ceb26b7e92f496a5b512
QuiMatematica <claudio@quimatematica.it>
2025-06-08 15:47:19 +0200

    Stoner in azione

--- src/pages/avanzato/stoner-trap/stoner-in-azione.php
+++ src/pages/avanzato/stoner-trap/stoner-in-azione.php
@@ -6,13 +6,14 @@
         Nelle centinaia di partite giocate ne ho subito pochissime, forse solo una, da Caspard
         in un torneo milanese degli anni '90.</i></p>
 
-<p>Ma poi ho cercato di ricordarmi quante Stoner trap avessi giocato nelle mie partite.</p>
+<p>Ho cercato di ricordarmi quante Stoner trap ho giocato nelle mie partite.</p>
 
-<p>Me ne ricordo molto bene una. 22 aprile 1990: avevo 15 anni. Ero al mio secondo torneo, in quel di Modena,
+<p>Me ne ricordo molto bene una. 22 aprile 1990: avevo 14 anni. Ero al mio secondo torneo, in quel di Modena,
 ospite dei simpaticissimi Angela Quattrocchi e Luca Ottani. Stavo giocando proprio contro Angela. E all'improvviso
-eccola lì: la possibilità di giocare la mia prima Stoner trap. E ho vinto la partita.</p>
+eccola lì: la possibilità di giocare la mia prima Stoner trap. E ho vinto la partita.
+    Purtroppo non ho più il referto, quindi non posso mostrarvela.</p>
 
-<p>Altre... no! Certo non ricordo a memoria tutte le mie partite, ma ho un ricordo di tutte le più significative.
+<p>Altre... no! Certo non ricordo a memoria tutte le mie partite, ma ho un ricordo più o meno vago di alcune più significative.
 E non ricordo nessun'altra Stoner trap.</p>
 
 <p>Ma allora è vero che sono così rare? Al punto che forse non vale la pena studiarle?</p>
@@ -23,8 +24,7 @@ E non ricordo nessun'altra Stoner trap.</p>
 della saga di Guerre Stellari.</p>
 
 <p>Anche se non si vedono, le Stoner trap sono spesso dietro l'angolo. Sono una minaccia che può concretizzarsi
-    da un momento all'altro. Ma sono minacce da cui abbiamo imparato a difenderci. Della serie:
-    <i>se le conosci le eviti</i>. E se le eviti non le vedi.</p>
+    da un momento all'altro. Ma <i>se le conosci le eviti</i>. E se le eviti, non si presentano in partita.</p>
 
 <p>Ma state certi che se, ad alto livello, un giocatore ottiene una formazione sbilanciata, subito entrambi pensano
 alla Stoner trap: l'uno per capire se può utilizzarla, l'altro per fare di tutto per evitarla.</p>
@@ -37,12 +37,48 @@ in una regione.</p>
 <p>Quindi tra due giocatori che conoscono bene il gioco, la Stoner trap può realizzarsi essenzialmente in due occasioni:</p>
 <ul>
     <li>chi può essere attaccato non si è accorto che ci sono tutte le condizioni per subire l'attacco;</li>
-    <li>chi può essere attaccato ha valutato meno dannoso l'attacco delle manovre necessarie a pararlo.</li>
+    <li>chi può essere attaccato ha valutato meno dannoso subire l'attacco rispetto alle manovre necessarie a evitarlo.</li>
 </ul>
+
+<h2>Evitare di sbilanciarsi</h2>
+
+<p>Primo giugno 2025, prima finale del Quarto Campionato Europeo, a Praga. La partita è tutta svizzera,
+    tra Matthias Berg (nero) e Arthur Juigner (bianco). Il diagramma <span data-board-ref="berg-juigner-2025-37"></span>
+    mostra la posizione dopo la mossa 37 del nero in <b>F8</b>.
+
+<board data-type="show" data-label="berg-juigner-2025-37" data-file="berg-juigner-2025-37.json"
+       data-caption="Berg - Juigner, Praga"></board>
+
+<p>Il bianco deve necessariamente rispondere sul bordo sud, pena la perdita di due tempi di gioco. Ha due mosse tra cui
+scegliere: incunearsi in <b>E8</b> o sbilanciarsi in <b>B8</b>.</p>
+
+<p>Giocando in <b>E8</b> si forma una posizione che è nota come <i>Suekuni trap</i>, che studieremo più avanti nel corso.
+E' una posizione svantaggiosa per il bianco, in particolare se si trova ad avere poche mosse: rischia di essere costretto
+    a giocare in <b>B8</b> o in <b>G8</b>, lasciando l'intero bordo all'avversario. In questo momento la mobilità del bianco
+è buona, anche se ha un'influenza piuttosto alta. Tuttavia non dobbiamo dimenticare che entrambe i colori avranno
+poche mosse nel finale, quando ci saranno effettivamente poche caselle libere. Per riassumere, diciamo che è una posizione
+    scomoda, non particolarmente pericolosa in questo momento, ma che potrebbe dare fastidio sul finale.</p>
+
+<p>Se invece il bianco si sbilancia in <b>B8</b>... il nero può partire subito con una Stoner trap. Ed è effettivamente
+    la sua risposta migliore. Il diagramma <span data-board-ref="berg-juigner-2025-37-ipotesi"></span> mostra
+    una possibile continuazione.</p>
+
+<board data-type="show" data-label="berg-juigner-2025-37-ipotesi" data-file="berg-juigner-2025-37-ipotesi.json"
+       data-caption="Berg - Juigner, Praga - Ipotesi con 38. <b>B8</b>."></board>
+
+<p>Sono convinto che Matthias abbia visto il pericolo della Stoner trap, per cui tra le due opzioni ha preferito giocare
+    in <b>E8</b>, nascondendo così una potenziale Stoner trap che, anche in questa occasione, non si è manifestata.
+E ha fatto bene. Abbiamo visto che subire la Stoner trap avrebbe portato il bianco alla sconfitta. Con <b>E8</b>,
+invece, ha vinto.</p>
+
+Puoi vedere il reale svolgimento della partita su flipthedisk
+<a href="https://flipthedisc.com/live/299/31499" target="_blank" rel="noopener noreferrer"><i class="bi bi-box-arrow-right"></i></a></p>.
+
 
 <h2>Una Stoner trap iniziata ma trasformata in qualcos'altro</h2>
 
-<p>Il diagramma 1 riporta la situazione dopo 29-esima mossa della partita tra l'inglese Imre Leader (nero) e
+<p>Il diagramma <span data-board-ref="leader-berg-2025-29"></span>
+    riporta la situazione dopo 29-esima mossa della partita tra l'inglese Imre Leader (nero) e
 il tedesco Matthias Berg (bianco) all'ottavo turno del torneo di Cambridge, valido per
     l'European Grand Prix 2024-2025.</p>
 
@@ -54,14 +90,8 @@ Inoltre ha a disposizione una mossa ideale in <b>G6</b>.</p>
 all'avversario la mossa <b>G6</b>. <b>B2</b> è una buona candidata. Si forma un bordo sbilanciato a nord, ma non è
 attaccabile con una Stoner trap. La mossa è fattibile.</p>
 
-<div class="card mx-auto board-card my-3">
-	<div class="card-body">
-		<div class="match-file-board" data-file="stoner-in-azione-1.json"></div>
-	</div>
-	<div class="card-footer text-body-secondary text-center">
-		Diagramma 1.
-	</div>
-</div>
+<board data-type="show" data-label="leader-berg-2025-29" data-file="stoner-in-azione-1.json"
+       data-caption="Leader - Berg, Cambridge."></board>
 
 Puoi vedere il resto della partita su flipthedisk
 <a href="https://flipthedisc.com/live/189/25822" target="_blank" rel="noopener noreferrer"><i class="bi bi-box-arrow-right"></i></a></p>.

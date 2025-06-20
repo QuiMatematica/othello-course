e5c5bc64f6d431bcbe5eba97712d527dc2179d1b
QuiMatematica <claudio@quimatematica.it>
2025-06-20 19:11:10 +0200

    Controllo di linea

--- src/pages/avanzato/controllo-di-linee/chapter.php
+++ src/pages/avanzato/controllo-di-linee/chapter.php
@@ -36,12 +36,33 @@
 Per cui se a uno dei suoi estremi c'è una casella vuota, e non ci sono altre direzioni
     lungo cui voltare pedine, non è possibile muovere in tale casella.</p>
 
-<p>Nel diagramma <span></span> se il nero gioca in <b>A8</b> controlla tutta la colonna <b>B</b>.
+<p>Nel diagramma <span data-board-ref="non-puo-girare"></span> se il nero gioca in <b>A8</b> controlla tutta la colonna <b>B</b>.
 Il bianco può giocare in <b>B8</b> (infatti volta pedine in diagonale) ma, per effetto della colonna controllata,
     non volta alcuna pedina in verticale</p>
 
 <board data-type="show" data-label="non-puo-girare" data-file="chapter-non-puo-girare.json"
        data-caption="Il nero controlla la colonna e il bianco non volta pedine lungo la stessa."></board>
 
-<p>In questi due esempi abbiamo visto il controllo di una linea come un aspetto positivo. In verità gli stessi
-effetti </p>
+<p>Se una linea è controllata da un colore, non può essere utilizzata per voltare pedine.
+    Per cui se a uno dei suoi estremi c'è una casella vuota:</p>
+<ul>
+    <li>se non ci sono altre direzioni lungo cui voltare pedine, non è possibile muovere in tale casella;</li>
+    <li>se ci sono altre direzioni lungo cui voltare pedine, si può giocare nella casella ma si voltano poche pedine.</li>
+</ul>
+
+<p>Ma il controllo di una linea può anche avere effetti negativi per il colore che lo detiene.</p>
+
+<p>Nel diagramma <span data-board-ref="perde-la-mossa"></span> il bianco ha il controllo della diagonale
+    <b>A1-G7</b>. Il nero può giocare in <b>H8</b> perché volta pedine in verticale, ma facendolo non volta la pedina
+<b>G7</b>. Quindi il bianco è costretto a passare e il nero vince la partita.</p>
+
+<board data-type="show" data-label="perde-la-mossa" data-file="chapter-perde-la-mossa.json"
+       data-caption="Il bianco controlla la diagonale quindi il nero può giocare in <b>H8</b> senza voltare <b>G7</b>."></board>
+
+<p>Visto che sono ininfluenti per quanto riguarda le mosse a disposizione e le direzioni di voltura, si parla
+di controllo di linea anche se le caselle sui bordi sono di un colore diverso purché separate dalla linea
+controllata da caselle vuote. Per esempio nel diagramma <span data-board-ref="dettaglio"></span> il bianco
+controlla il controbordo sud, anche se le pedine <b>A7</b> e <b>H7</b> sono nere.</p>
+
+<board data-type="show" data-label="dettaglio" data-file="chapter-dettaglio.json"
+       data-caption="Il bianco controlla il controbordo sud."></board>

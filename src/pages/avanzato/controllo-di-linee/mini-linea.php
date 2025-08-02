70cfcc2a50762d4b1fa5a73efc20a3f0c3ffc6b4
QuiMatematica <claudio@quimatematica.it>
2025-08-02 12:12:38 +0200

    Controllo di mini-linee

--- src/pages/avanzato/controllo-di-linee/mini-linea.php
+++ src/pages/avanzato/controllo-di-linee/mini-linea.php
@@ -14,14 +14,24 @@ maggiore rispetto alle <i>mini-righe</i> e <i>mini-colonne</i>.</p>
 
 <p>Il controllo delle mini-linee in apertura ha gli stessi effetti del controllo delle linee critiche nel finale.</p>
 
-<h2>Un esempio</h2>
+<div class="card border-primary mb-3">
+	<div class="card-header">Definizione</div>
+	<div class="card-body">
+		<p class="card-text">Chiamiamo <b>mini-linea</b> una linea controllate da un colore
+            formata da due, tre o quattro pedine.</p>
+        <p class="card-text">Chiamiamo <b>mini-diagonale</b> una mini-linea di pedine in diagonale.</p>
+        <p class="card-text">Chiamiamo <b>ordine</b> il numero di pedine che formano la mini-linea.</p>
+	</div>
+</div>
+
+<h2>Perché sono importanti</h2>
 
 <p>Osserva la posizione del diagramma <span data-board-ref="esempio-1"></span>.</p>
 
 <board data-type="show" data-label="esempio-1" data-file="mini-linee-esempio-1.json"></board>
 
-<p>Le pedine <b>D5</b> ed <b>E4</b> sono nere e formano una piccola diagonale controllata dal nero.
-Nel diagramma <span data-board-ref="esempio-1-evidenzia"></span> ho evidenziato la diagonale con il tratto nero.</p>
+<p>Le pedine <b>D5</b> ed <b>E4</b> formano una mini-diagonale di ordine 2 controllata dal nero.
+Nel diagramma <span data-board-ref="esempio-1-evidenzia"></span> ho evidenziato la mini-diagonale con il tratto nero.</p>
 
 <board data-type="show" data-label="esempio-1-evidenzia" data-file="mini-linee-esempio-1-evidenzia.json"></board>
 
@@ -54,13 +64,30 @@ Ne vedi il risultato nei diagrammi <span data-board-ref="esempio-1-B4"></span> e
     arriveranno altre pedine sulla linea stessa e più difficile sarà mantenere il controllo.
 Tuttavia più a lungo si riuscirà a mantenere questo controllo maggiore sarà il vantaggio derivante.</p>
 
-<h2>Un altro esempio</h2>
+<h2>Esempi di aperture con mini-diagonali</h2>
 
 <p>Nel primo esempio la situazione era sicuramente a favore del nero. Ma queste mini-linee, in particolare le
-mini-diagonali, compaiono anche in aperture molto più equilibrate. Guarda, per esempio, la sequenza proposta nel
-diagramma <span data-board-ref="rose-bill"></span>.</p>
+    mini-diagonali, compaiono anche in aperture molto più equilibrate.</p>
+
+<p>Il diagramma <span data-board-ref="rose-bill"></span> ti mostra una sequenza considerata pari di un'apertura
+    molto nota e molto giocata.</p>
 
 <board data-type="show" data-label="rose-bill" data-file="mini-linee-rose-bill.json"></board>
+
+<p>I diagrammi <span data-board-ref="keisuke"></span>, <span data-board-ref="mako"></span> e
+<span data-board-ref="leader"></span> mostrano varianti dell'apertura Tigre presentata nel diagramma
+<span data-board-ref="rose-bill"></span>. Come vedrai in tutte queste aperture l'attenzione del nero si
+concentra nella formazione di mini-diagonali. (Questi esempi sono stati proposti dal maestro
+Mauro Perotti.)</p>
+
+<gather>
+    <board data-type="show" data-label="keisuke" data-file="mini-linee-keisuke.json"></board>
+    <board data-type="show" data-label="mako" data-file="mini-linee-mako.json"></board>
+    <board data-type="show" data-label="leader" data-file="mini-linee-leader.json"></board>
+</gather>
+
+<p>Quello che emerge è che, nelle prime fasi dell'apertura, il controllo di una mini-linea (in particolare
+di una mini-diagonale) e il controllo del centro sembra strategie sovrapponibili.</p>
 
 <h2>Riassumendo</h2>
 

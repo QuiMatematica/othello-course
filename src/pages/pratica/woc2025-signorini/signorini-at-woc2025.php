32fbfe94c0a0488e9d5d029e948457a47a567206
QuiMatematica <claudio@quimatematica.it>
2025-12-14 20:20:28 +0100

    WOC 2025

---
+++ src/pages/pratica/woc2025-signorini/signorini-at-woc2025.php
@@ -0,0 +1,136 @@
+<p>Il 47-esimo Campionato Mondiale di Othello (<i>World Othello Championship</i> - <i>WOC</i>)
+si è disputato ad Ankara (Turchia) dal 12 al 15 novembre 2025, presso il modernissimo Ankara Hotel,
+costruito sopra la stazione ferroviaria della città.</p>
+
+<p>Ho avuto la possibilità di partecipare a questo mondiale grazie a un anno othellisticamente parlando piuttosto
+positivo.</p>
+
+<ul>
+    <li>Ho concluso il GPI (Gran Prix Italiano) 2025 in sesta posizione, grazie al quarto posto a Ferrara e al secondo posto a Udine.</li>
+    <li>Dopo il torneo di Udine ero in 13-esima posizione della classifica rating italiana.</li>
+    <li>Con lo stesso torneo di Udine ho ottenuto il titolo di Maestro della FNGO.</li>
+</ul>
+
+<p>Grazie a tutto questo, e alla defezione di altri giocatori che avevano precedenza, sono stato invitato a partecipare
+al mondiale.</p>
+
+<p>Quello del 2025 è il mio secondo mondiale. Il primo è stato nel 2005, a Reykjavik (Islanda).</p>
+
+<div class="table-responsive">
+  <table class="table table-striped table-hover align-middle">
+    <thead class="table-dark">
+      <tr>
+        <th scope="col" class="text-center">#</th>
+        <th scope="col">Nero</th>
+        <th scope="col" class="text-center">Punteggio</th>
+        <th scope="col">Bianco</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="text-center">1</td>
+        <td><strong>Pihlajapuro Katie</strong></td>
+        <td class="text-center">
+          <a href="01-pihlajapuro-signorini.php"><strong>48 – 16</strong></a>
+        </td>
+        <td>Signorini Claudio</td>
+      </tr>
+      <tr>
+        <td class="text-center">2</td>
+        <td>Signorini Claudio</td>
+        <td class="text-center">
+          <a href="02-signorini-kurita.php"><strong>12 – 52</strong></a>
+        </td>
+        <td><strong>Kurita Seiya</strong></td>
+      </tr>
+      <tr>
+        <td class="text-center">3</td>
+        <td>Kjellberg Arvid</td>
+        <td class="text-center">
+          <a href="03-kjellberg-signorini.php"><strong>17 – 47</strong></a>
+        </td>
+        <td><strong>Signorini Claudio</strong></td>
+      </tr>
+      <tr>
+        <td class="text-center">4</td>
+        <td>Signorini Claudio</td>
+        <td class="text-center">
+          <a href="04-signorini-gornert.php"><strong>24 – 40</strong></a>
+        </td>
+        <td><strong>Gornert Daniel</strong></td>
+      </tr>
+      <tr>
+        <td class="text-center">5</td>
+        <td>Aagaard-Hansen Jens</td>
+        <td class="text-center">
+          <a href="05-aagaardhansen-signorini.php"><strong>15 – 49</strong></a>
+        </td>
+        <td><strong>Signorini Claudio</strong></td>
+      </tr>
+      <tr>
+        <td class="text-center">6</td>
+        <td><strong>Signorini Claudio</strong></td>
+        <td class="text-center">
+          <a href="06-signorini-qvist.php"><strong>38 – 26</strong></a>
+        </td>
+        <td>Qvist Jessen Claus</td>
+      </tr>
+      <tr>
+        <td class="text-center">7</td>
+        <td><strong>Takigawa Mitsuaki</strong></td>
+        <td class="text-center">
+          <a href="07-takigawa-signorini.php"><strong>41 – 23</strong></a>
+        </td>
+        <td>Signorini Claudio</td>
+      </tr>
+      <tr>
+        <td class="text-center">8</td>
+        <td><strong>Signorini Claudio</strong></td>
+        <td class="text-center">
+          <a href="partite/woc2025_round8_signorini_perelman.html"><strong>42 – 22</strong></a>
+        </td>
+        <td>Perel’man Andrey</td>
+      </tr>
+      <tr>
+        <td class="text-center">9</td>
+        <td>Yang Yun Fan</td>
+        <td class="text-center">
+          <a href="partite/woc2025_round8_signorini_perelman.html"><strong>19 – 45</strong></a>
+        </td>
+          <td><strong>Signorini Claudio</strong></td>
+      </tr>
+      <tr>
+        <td class="text-center">10</td>
+        <td><strong>Signorini Claudio</strong></td>
+        <td class="text-center">
+          <a href="partite/woc2025_round8_signorini_perelman.html"><strong>42 – 22</strong></a>
+        </td>
+        <td>Douda Tomas</td>
+      </tr>
+      <tr>
+        <td class="text-center">11</td>
+        <td><strong>Schotte Tom</strong></td>
+        <td class="text-center">
+          <a href="partite/woc2025_round8_signorini_perelman.html"><strong>43 – 21</strong></a>
+        </td>
+        <td>Signorini Claudio</td>
+      </tr>
+      <tr>
+        <td class="text-center">12</td>
+        <td>Signorini Claudio</td>
+        <td class="text-center">
+          <a href="partite/woc2025_round8_signorini_perelman.html"><strong>23 – 41</strong></a>
+        </td>
+        <td><strong>Kobzev Vyacheslav</strong></td>
+      </tr>
+      <tr>
+        <td class="text-center">13</td>
+        <td><strong>Leader Imre</strong></td>
+        <td class="text-center">
+          <a href="partite/woc2025_round8_signorini_perelman.html"><strong>38 – 26</strong></a>
+        </td>
+        <td>Signorini Claudio</td>
+      </tr>
+    </tbody>
+  </table>
+</div>

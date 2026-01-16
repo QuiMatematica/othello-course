95fc9d20b0315b0334d4265928dd45f74d8fe6a1
QuiMatematica <claudio@quimatematica.it>
2026-01-16 11:54:45 +0100

    Swindle

--- src/pages/avanzato/swindle/quiz.php
+++ src/pages/avanzato/swindle/quiz.php
@@ -3,3 +3,7 @@
 <board data-type="quiz" data-label="quiz2" data-file="quiz2.json" ></board>
 
 <board data-type="quiz" data-label="quiz3" data-file="quiz3.json" ></board>
+
+<board data-type="quiz" data-label="quiz4" data-file="quiz4.json" ></board>
+
+<board data-type="quiz" data-label="quiz5" data-file="quiz5.json" ></board>

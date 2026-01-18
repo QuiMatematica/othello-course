eb7fcfff9f62e59e6ac54a180e7d49b18fc2cdff
QuiMatematica <claudio@quimatematica.it>
2026-01-18 18:19:04 +0100

    Swindle celebri

---
+++ src/pages/avanzato/swindle-celebri/landau.php
@@ -0,0 +1,10 @@
+<p>Ted Landau è stato un campione americano negli anni 80.</p>
+
+<p>La posizione del diagramma <span data-board-def="landau-1"></span> è tratta da un gioco via posta (quando
+Internet ancora non esisteva) dove Landau giocava con il nero. Tu dove giocheresti?</p>
+
+<p>Segui la sequenza proposta, passata alla storia come <i>manovra Landau</i>.</p>
+
+<board data-type="show" data-label="landau-1" data-file="landau-1.json"></board>
+
+<p>Analizziamo insieme quali sono gli elementi che hanno permesso la manovra.</p>

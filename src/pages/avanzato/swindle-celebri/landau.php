d023e1070db98deede9641be138c12aad1d32d15
QuiMatematica <claudio@quimatematica.it>
2026-01-22 15:42:31 +0100

    Swindle celebri

--- src/pages/avanzato/swindle-celebri/landau.php
+++ src/pages/avanzato/swindle-celebri/landau.php
@@ -7,4 +7,29 @@ Internet ancora non esisteva) dove Landau giocava con il nero. Tu dove giocheres
 
 <board data-type="show" data-label="landau-1" data-file="landau-1.json"></board>
 
-<p>Analizziamo insieme quali sono gli elementi che hanno permesso la manovra.</p>
+<p>Una manovra così complessa ha bisogno, chiaramente di diversi elementi che concorrono alla sua riuscita.</p>
+
+<p>L'elemento fondamentale, ed è quello che su poi si incentrano le manovre Landau, è il controllo del
+controbordo. E tale controllo non deve essere improvvisato, ma va costruito tipicamente andando a cedere
+il bordo relativo.</p>
+
+<p>Quindi, tornando alla posizione di partenza (diagramma <span data-board-ref="landau-1-controbordo"></span>,
+    il primo elemento da cogliere è come le mosse in <b>A5</b> e
+in <b>A7</b> abbiamo sì concesso il bordo al giocatore attaccato (frecce gialle), ma abbiamo reso possibile il controllo del
+controbordo (frecce rosse).</p>
+
+<board data-type="show" data-label="landau-1-controbordo" data-file="landau-1-controbordo.json"></board>
+
+<p>Il secondo elemento (diagramma <span data-board-ref="landau-1-accessi"></span>)
+    è dato dalle condizioni adatta per giocare lo swindle vero e proprio una volta che il
+bordo è stato ceduto e il controbordo conquistato. In particolare:</p>
+<ul>
+    <li>il colore attaccato non deve avere mosse a disposizioni nelle caselle rimaste liberi, e questo
+    è garantito dal fatto che le pedine della colonna <b>3</b> adiacenti sono tutte del colore attaccato
+    (rettangoli giallo);</li>
+    <li>il colore attaccante deve aver la possibilità di giocare nelle caselle <b>X</b> senza concedere la
+    replica all'avversario nelle caselle <b>C</b> adiacenti, e questo è garantito dal fatto che l'attaccato
+    controlla i controbordi (rettangoli rossi).</li>
+</ul>
+
+<board data-type="show" data-label="landau-1-accessi" data-file="landau-1-accessi.json"></board>

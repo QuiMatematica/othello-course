0df0bcf55be444e6f4084fbbdd0409cba7c278ec
QuiMatematica <claudio@quimatematica.it>
2026-01-25 17:24:22 +0100

    Affatigato

---
+++ src/pages/avanzato/approfondimenti/contare.php
@@ -0,0 +1,101 @@
+<div class="d-flex align-items-center text-muted small my-3">
+  <i class="bi bi-person-circle me-2"></i>
+  <span>
+    di <strong class="text-dark">Carlo Affatigato</strong>
+  </span>
+    &nbsp; - &nbsp;
+  <i class="bi bi-calendar3 me-1"></i>
+    <span>
+        <time datetime="2026-01-24">24 gennaio 2026</time>
+    </span>
+</div>
+
+<p class="small-caps"><b>Spoiler: la risposta non è un numero</b></p>
+
+<p>Una volta, quando ero giovane e ingenuo e studiavo ancora dai libri per diventare bravo a scacchi, lessi di quella
+    famosa citazione dello scacchista cecoslovacco Richard Retí, che quando gli si chiedeva quante mosse calcolava
+    normalmente in partita, rispondeva "di norma, nemmeno una." Lui ci teneva a dire che era sincero quando rispondeva
+    così. Quella risposta aveva un senso ben preciso: serviva a sottolineare che la pura predizione non è l'unico metodo
+    per giocare bene, né necessariamente il più importante, ma che esistono altri approcci che da umani usiamo per
+    eccellere in giochi complessi come scacchi, Othello, o Go.</p>
+
+<div class="card mx-auto my-3" style="width: fit-content; max-width: 100%;">
+	<div class="card-body">
+		<img src="othello-thinking.png" alt="Othello thinking" class="card-img-top img-fluid">
+	</div>
+</div>
+
+<h2>L'artista e l'ingegnere</h2>
+
+<p>Concentrare l'attenzione sulle nostre capacità predittive, sull'abilità di vedere la scacchiera avanti nel tempo,
+    è un gioco del tutto lecito. Soprattutto in tempi moderni, in cui vediamo i computer e le intelligenze artificiali
+    eccellere nei giochi che amiamo per la loro ampia potenza di calcolo. La verità, però, è che esistono due approcci
+    possibili per valutare la migliore mossa a Othello (o in qualsiasi altro gioco complesso astratto):</p>
+<ul>
+    <li><b>l'approccio posizionale</b>, che analizza i pattern e i punti di forza della posizione sull'othelliera e
+        identifica una o più mosse che puntano principalmente a creare una posizione buona, o che comunque ci dia
+        confidenza;</li>
+    <li><b>l'approccio predittivo</b>, che valuta in sede separata un certo numero di mosse possibili e per ognuna di esse
+        porta avanti la sequenza "a mente", predicendo le possibili risposte avversarie, approfondendo alberi
+        decisionali più o meno lunghi, e spesso contando le pedine guadagnate man mano che ci portiamo avanti
+        nell'analisi.</li>
+</ul>
+
+<p>Per certi versi, li si può pensare come la classica distinzione tra il "gioco d'istinto" e il "ragionamento."
+    Nel mondo dei giochi spesso li si identifica come stile "romantico/artistico" e stile "analitico/ingegneristico".
+    La teoria dei giochi chiama il primo "euristica," da contrapporsi all'analisi esaustiva. Daniel Kahneman
+    impugnerebbe il suo libro "Pensieri Lenti e Veloci" e direbbe che si tratta semplicemente di quelli che lui ha
+    battezzato "sistema di pensiero 1 e 2." Tutti modi validi e complementari di dire la stessa cosa.</p>
+
+<h2>Se fossi un computer...</h2>
+
+<p>Entrambi gli approcci raggiungono un obiettivo, e se raffinati, portano a risultati consistentemente buoni.
+    L'approccio predittivo, però, è <b>estremamente dispendioso in termini di energia mentale</b>: consuma energie in maniera
+    massiccia e può portarci a una situazione di stanchezza difficile da recuperare in tempi rapidi. Per questo
+    ricorriamo frequentemente all'approccio posizionale, che nella sua accezione più pura è "assenza di ragionamento":
+    il cervello fornisce una risposta immediata, senza ragionare, come risultato dei pattern riconosciuti dalle
+    esperienze passate, dalle sequenze viste in casi simili, oppure (riprendendo un tema caldo del momento)
+    dalle linee memorizzate in fase di studio (la memoria non usa capacità di calcolo). Più siamo stanchi,
+    più facilmente il nostro cervello ricorre all'approccio posizionale. Il rischio, però, è che la visione posizionale
+    può nascondere tranelli che non si vedono subito, risposte inaspettate o trappole che il nostro avversario
+    può giocare, portando la partita a proprio favore.</p>
+
+<p>Se fossimo computer, non faremmo altro che usare l'approccio predittivo ad ogni mossa, spingendo il nostro cervello
+    più in là possibile ogni volta che tocca a noi. Questo equivarrebbe a calcolare in anticipo di quanti gradi
+    esattamente dovremo girare il volante della nostra auto prima di affrontare ogni curva: non solo umanamente
+    impossibile, ma completamente inefficace in termini di rapporto sforzo/risultato. Essendo umani, siamo più bravi
+    dei computer in un'altra cosa: quella di valutare una situazione "a occhio," ottenendo un'interpretazione generale
+    della bontà di una mossa in maniera pressoché immediata, sebbene con un certo margine di errore. I computer
+    direbbero: "se fossi umano, eviterei di calcolare in maniera bruta tutti i rami dell'albero decisionale ed
+    eliminerei a priori una buona metà di mosse insensate "per istinto", ma ahimé, l'istinto non ce l'ho!" Mentre noi
+    sudiamo sette camicie per migliorare le nostre abilità analitiche, i computer darebbero via metà della loro
+    capacità computazionale in cambio delle nostre abilità "artistiche/romantiche" e delle nostre valutazioni
+    immediate.</p>
+
+<h2>Il gambetto del glucosio: è un mondo molto molto rischioso</h2>
+
+<p>A questo punto, dovrebbe essere chiaro dove andremo a parare con questo articolo che parte con una domanda e per
+    3/4 del testo fa di tutto per evitare di rispondere. Ridurre la nostra bravura ad Othello ad una misura numerica
+    delle mosse che prediciamo è fuorviante e dannoso. Giocare bene a Othello significa trovare <b>il mix ideale tra
+        approccio analitico e posizionale</b>. Non conta tanto quante mosse siamo in grado di predire, ma quanto bravi siamo
+    a stabilire quando è il momento di calcolare, e quando è meglio risparmiare le energie e fidarci della visione
+    posizionale. L'esperienza dei grandi giocatori gli permette di riconoscere meglio degli altri quando è il momento
+    di fermarsi e ragionare, conoscendo bene lo spazio che possono concedere alla fase di calcolo. L'approccio
+    posizionale torna utile per scremare le possibilità che è necessario approfondire in maniera analitica,
+    e i giocatori esperti sono quelli che hanno trovato un'equilibrio migliore tra i due approcci.</p>
+
+<p>Più grande è l'esperienza, più facile è riconoscere in una posizione il rischio di fermarsi a una analisi
+    superficiale. L'esperienza serve anche a limitare le "direzioni" in cui l'analisi si porterà, e a saper giudicare
+    quando va bene fermare il processo predittivo. Giocare a Othello è sempre una battaglia contro il tempo, oltre che
+    contro i nostri limiti computazionali, quindi il vero talento non è il numero di mosse che sappiamo calcolare, ma
+    sapere quando approfondire e quando fermarsi. Un mix affascinante di gestione emotiva, preparazione e conoscenza
+    dei propri limiti e punti di forza: <b>migliorarsi non significa per forza aumentare la profondità delle nostre
+        valutazioni</b>, ma essere in grado di identificare con maggiore precisione quando l'analisi è sufficiente,
+    quando è eccessiva, e quando è carente, e stabilire l'approccio migliore in termini di rischio di fare una mossa
+    perdente, rischio di finire il tempo e rischio di finire il glucosio che circola nel nostro corpo.</p>
+
+<p>È un mondo pieno di rischi, insomma, e barcamenarcisi è praticamente impossibile. I giocatori "di esperienza" sono
+    spesso più bravi di noi a Othello, ma capire in cosa sono più bravi non è facile (e se glielo chiedi, non sanno
+    cosa risponderti). Quindi ci tocca studiare e studiare, approfondire i nostri limiti e le caratteristiche
+    del ragionamento umano, provare approcci diversi per testarne la validità, e... giocare, giocare, e giocare ancora.
+    Che ad accumulare esperienza di sicuro non si sbaglia.</p>

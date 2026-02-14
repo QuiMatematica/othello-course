e2c19f100c0761d95ac24b3cf1235341a91173f7
QuiMatematica <claudio@quimatematica.it>
2026-02-14 15:27:37 +0100

    Generazione database

--- src/web/service-worker.js
+++ src/web/service-worker.js
@@ -56,3 +56,23 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
+// Notifiche push
+self.addEventListener("push", event => {
+  const data = event.data.json();
+
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: "/icons/icon-192.png",
+      data: { url: data.url }
+    })
+  );
+});
+
+self.addEventListener("notificationclick", event => {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data.url)
+  );
+});

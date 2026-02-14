97e0279134c9baaa12277fae2f9125086b78c01e
QuiMatematica <claudio@quimatematica.it>
2026-02-14 15:46:18 +0100

    Notifiche push

--- src/web/api/get-vapid-public-key.php
+++ src/web/api/get-vapid-public-key.php
@@ -1,5 +1,5 @@
 <?php
-$config = require __DIR__ . '/../config/push-config.php';
+$config = require __DIR__ . '/../../config/push-config.php';
 
 header('Content-Type: application/json');
 echo json_encode([

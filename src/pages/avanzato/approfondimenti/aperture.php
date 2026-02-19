afb9e294ec18a00f10628e6d538d7305ff5cd439
QuiMatematica <claudio@quimatematica.it>
2026-02-19 20:57:13 +0100

    Affatigato

---
+++ src/pages/avanzato/approfondimenti/aperture.php
@@ -0,0 +1,85 @@
+<div class="d-flex align-items-center text-muted small my-3">
+  <i class="bi bi-person-circle me-2"></i>
+  <span>
+    di <strong class="text-dark">Carlo Affatigato</strong>
+  </span>
+    &nbsp; - &nbsp;
+  <i class="bi bi-calendar3 me-1"></i>
+    <span>
+        <time datetime="2026-02-18">18 febbraio 2026</time>
+    </span>
+</div>
+
+<p>Ciao mamma, guarda! Sono grande ormai! Sono andato attraverso tutte le classiche fasi di crescita, ho fatto i miei
+    capricci, avuto le mie crisi adolescenziali, ma ora sento di poter sedere al tavolo degli adulti. Cosa dici? Devo
+    ancora stare con i cuginetti? Ma io voglio scambiare punti di vista con gli zii, mi sento pronto. Lo giuro, ne sono
+    fuori: la mia dipendenza dalle aperture è finita! Sono "pulito" da oltre tre settimane!</p>
+
+<div class="card mx-auto my-3" style="width: fit-content; max-width: 100%;">
+	<div class="card-body">
+		<img src="aperture.png" alt="Il giorno in cui sono evaso dalla prigione delle aperture" class="card-img-top img-fluid">
+	</div>
+</div>
+
+<p>Sì, la logica che sta dietro alle aperture nell'Othello è molto simile a quella di una tossicodipendenza. Conoscere
+    bene le aperture può portare un vantaggio enorme, non fraintendetemi. Soprattutto oggi, con tutti i software
+    disponibili che rendono facile e veloce studiare l'efficacia di una mossa all'inizio della partita, invece di
+    perdere ore valutando ogni variante sul tavolo. Inizi a giocare a Othello e la prima cosa che vuoi fare è giocare
+    il più vicino possibile a un computer. Il che significa fare sempre le "mosse giuste", specialmente all'inizio. Ti
+    appassioni, magari ti rendi conto di avere una buona memoria e una discreta capacità di riconoscimento degli schemi
+    e, in pochi mesi, potresti finire per conoscere le aperture meglio di giocatori con decenni di esperienza. La cosa
+    ti entusiasma, perché capisci che se riesci a trascinare quei giocatori in una situazione equilibrata a 20 mosse
+    dalla fine, loro hanno solo una manciata di possibilità di vincere la partita. Per loro è un vero azzardo, e spetta
+    a loro uscirne, magari rischiando un -6 da qualche parte a metà partita. Voglio dire, senza la mia ossessione per
+    le aperture, non avrei pareggiato contro David Hand l'anno scorso.</p>
+
+<board data-type="show" data-label="aperture-1" data-file="aperture-1.json"></board>
+
+<p>Tutto molto bello. Poi però ti rendi conto che è una prigione. E uscire dalla prigione delle aperture è parecchio
+    difficile.</p>
+
+<p>Ti rendi conto di dover uscire quando capisci che questa logica ha dei limiti. Alex Koh me lo disse anni fa, con la
+    tipica diplomazia asiatica: "Se continui su questa strada, forse un giorno sarai tra i migliori al mondo. Ma è una
+    strada lunga. E potrebbe essercene una più veloce..." Allora era troppo presto, non comprendevo appieno l'urgenza
+    di abbandonare l'unica cosa che mi dava sicurezza nel mio stile di gioco. Ma la gente continuava a dirmelo. Qualche
+    tempo dopo, Matthias Berg me la lanciò come una sfida e, siccome mi piacciono le sfide, funzionò. Poi un giorno
+    sono andato a Wrocław a giocare contro i forti giocatori polacchi, e Milosz Cupial mi spiegò perché avevo perso:
+    "Mi ero reso conto che conoscevi molto bene le aperture, quindi ho dovuto fare una mossa che ti spiazzasse,
+    abbastanza presto da avere il tempo di recuperare lo svantaggio, sperando che tu facessi un errore. Sai, gioco a
+    Othello da vent'anni..."</p>
+
+<board data-type="show" data-label="aperture-2" data-file="aperture-2.json"></board>
+
+<p>Così ho capito che, in fondo, è facile smantellare il vantaggio delle aperture. Un giocatore di esperienza potrebbe
+    facilmente sapere l'apertura più a lungo di te, oppure conoscere più varianti di te, o ancora avere la confidenza
+    giusta per uscire dal book presto e poi giocarsela in stile libero. E dall'altro lato, se tu hai scommesso così
+    tanto sulla memorizzazione di aperture lunghe, finisci per avere più problemi di autostima nel gioco libero, quindi
+    fuori dalle sequenze memorizzate. Era così che mi sentivo, e, che diamine, avevo bisogno di scoprire se avevo un
+    vero talento per l'Othello o se ero arrivato fin qui solo grazie alla memoria e a un po' di riconoscimento di
+    pattern.</p>
+
+<p>E così, il mio complicato rapporto con le aperture mi ha portato alla gioia di giocare XOT. Sì, lo so, molti di voi
+    ci erano già arrivati, ma abbiate pazienza, ho solo sei anni (anni-Othello, intendo). L'ho scoperto da poco.
+    Recentemente c'è stata persino la possibilità di giocare partite XOT da 1 minuto su Othello Quest e, beh, non mi
+    sono rimaste molte scuse per non provare. E con mia piacevole sorpresa, ho scoperto di non essere affatto male.
+    Ho i miei strumenti. "Vedo cose", come in un film di Shyamalan. Metto in difficoltà giocatori esperti. Ho persino
+    vinto il torneo XOT in Italia nel 2025, contro giocatori che mi danno sempre filo da torcere nelle partite
+    regolari.</p>
+
+<p>Tutti questi segnali mi dicevano che era ora di cambiare le cose. Ho iniziato a pensarci più seriamente, trattandola
+    come un'analisi da completare. La vera domanda a quel punto era: di quanto scende la mia percentuale di vittorie se,
+    invece di giocare "le aperture che so", tiro fuori subito qualcosa di folle a mossa 4 e giochiamo tutti allegramente
+    fuori dal book per tutta la partita?</p>
+
+<p>Non vi darò la risposta, ma a giudicare dal mio entusiasmo, potete probabilmente indovinarla. Quello che ho capito
+    è che giocare un -8 alla sesta mossa non è poi così diverso dal seguire le aperture che conosci e arrivare alla
+    mossa 45 a -4. Se sei un giocatore esperto, sopravvivere alle ultime quindici mosse potrebbe essere in realtà più
+    facile che gestire qualcosa di insolito fin dall'inizio. E so che prima di metà partita un computer direbbe che
+    sono in netto svantaggio. Ma chi se ne frega, tanto sto giocando contro un essere umano.</p>
+
+<p>Quindi, il giorno esatto in cui ho vinto la mia dipendenza dalle aperture... beh, non so di preciso quando è stato,
+    ma so che ora mi sento finalmente libero. E non ho problemi a svelarvi pubblicamente il mio nuovo stile di gioco,
+    tanto ormai ho sbloccato la consapevolezza di poter inventare una nuova variante praticamente ogni settimana.</p>
+
+<p>Anzi, se proprio vi ispira studiarvi due o tre aperture "folli" giocate pochissimo ma con varianti interessanti che
+    possono sorprendere l'avversario... restate lì. Ne parliamo di nuovo tra qualche giorno.</p>

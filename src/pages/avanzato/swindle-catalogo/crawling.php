9a68c7730909f3c12d4b02fbec4dd8da15ebd7c6
QuiMatematica <claudio@quimatematica.it>
2026-02-27 18:51:27 +0100

    Catalogo di swindle

--- src/pages/avanzato/swindle-catalogo/crawling.php
+++ src/pages/avanzato/swindle-catalogo/crawling.php
@@ -1,3 +1,10 @@
+<p>Questo pattern è conosciuto come <b>strisciare lungo i bordi</b>, in inglese
+<i>crawling on the edges</i>.</p>
+
+<p>   L'esempio è tratto dalla master class di Makoto Suekuni
+    <a href="https://www.youtube.com/watch?v=adk3sTpatQ8" target="_blank" rel="noopener noreferrer"><i class="bi bi-box-arrow-right"></i></a>.
+</p>
+
 <p>Nel diagramma <span data-board-ref="crawling"></span> il bianco ha preso tre bordi su quattro. Tocca al nero
 che deve cercare la strategia corretta per massimizzare il guadagno.</p>
 
@@ -7,5 +14,4 @@ dall'avversario: quello a sud.</p>
 
 <board data-type="show" data-label="crawling" data-file="crawling.json"></board>
 
-<p>Questo pattern è conosciuto come <b>strisciare lungo i bordi</b>, in inglese
-<i>crawling on the edges</i>.</p>
+<board data-type="show" data-label="crawling2" data-file="crawling2.json"></board>

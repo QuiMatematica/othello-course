132c4a7c9b4f04cb45f5bfd4c83aab7d4d13cd61
QuiMatematica <claudio@quimatematica.it>
2026-02-27 13:30:21 +0100

    Catalogo di swindle

---
+++ src/pages/avanzato/swindle-catalogo/crawling.php
@@ -0,0 +1,11 @@
+<p>Nel diagramma <span data-board-ref="crawling"></span> il bianco ha preso tre bordi su quattro. Tocca al nero
+che deve cercare la strategia corretta per massimizzare il guadagno.</p>
+
+<p>Il nero non può giocare il <a href="vittoria-senza-angoli.php">pattern precedente</a> perché il bianco non
+ha il controllo di due controbordi adiacenti. Ma il nero può comunque sfruttare l'unico controbordo controllato
+dall'avversario: quello a sud.</p>
+
+<board data-type="show" data-label="crawling" data-file="crawling.json"></board>
+
+<p>Questo pattern è conosciuto come <b>strisciare lungo i bordi</b>, in inglese
+<i>crawling on the edges</i>.</p>

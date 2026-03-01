9277adc2feb4ba44398db1b1d9caca3382fbf415
QuiMatematica <claudio@quimatematica.it>
2026-03-01 12:43:00 +0100

    Pattern e tesuji

---
+++ src/pages/avanzato/pattern1/pattern-e-tesuji.php
@@ -0,0 +1,87 @@
+<p>Non è esterofilia, ma ci sono alcuni termini che ormai sono entrati nel lessico comune del gioco, e se venissero tradotti
+    perderebbero il loro potere evocativo.</p>
+
+<div class="card border-primary mb-3">
+    <div class="card-header">Definizione</div>
+    <div class="card-body">
+        <p class="card-text">Con <b><i>pattern</i></b> indichiamo uno schema, una configurazione ricorrente.</p>
+    </div>
+</div>
+
+<p>Sono <i>pattern</i>, per esempio, i cinque sui bordi e le configurazione di bordo in generale.
+    Sono <i>pattern</i> anche le posizioni in cui è presente il controllo di una linea o di una diagonale.</p>
+
+<p>&Egrave; importante conoscere i <i>pattern</i> perché la stessa configurazione, o una simile con le stesse
+    caratteristiche,
+    va sempre affrontata nello stesso modo. Sapere cosa fare quando si presenta un determinato <i>pattern</i> permette
+    di
+    individuare più facilmente e velocemente la mossa giusta da applicare.</p>
+
+<div class="card border-primary mb-3">
+    <div class="card-header">Definizione</div>
+    <div class="card-body">
+        <p class="card-text"><b><i>Tesuji</i></b> (si pronuncia <i>tesugi</i>) è una parola giapponese usata in giochi
+            come il Go e lo Shogi. La possiamo tradurre come <i>sequenza di mosse brillanti</i>. I <i>tesuji</i> sono le
+            mosse giuste
+            da giocare quando si presenta un certo <i>pattern</i>.</p>
+    </div>
+</div>
+
+<p>Un <i>tesuji</i>, per esempio, è una Stoner trap applicata a un quattro sbilanciato. Il quattro sbilanciato è il <i>pattern</i>.
+    La Stoner trap è il <i>tesuji</i>: la sequenza di mosse brillanti che va a sfruttare le proprietà del <i>pattern</i>.
+</p>
+
+<p>Uno stesso <i>pattern</i> può portare a più di un <i>tesuji</i>. Per esempio abbiamo visto come un cinque può essere
+    attaccato sia da una casella X sia da una casella C.</p>
+
+<h2><i>Pattern</i> e <i>tesuji</i> già analizzati</h2>
+
+<p>Nei capitoli precedenti abbiamo già visto alcuni <i>pattern</i> e relativi <i>tesuji</i>.</p>
+
+<div class="container-fluid d-flex justify-content-center">
+    <div class="w-100 w-lg-50">
+        <div class="table-responsive">
+            <table class="table table-striped align-middle mb-0">
+                <thead>
+                <tr>
+                <tr>
+                    <th style="background-color: #198754; color: white;"><i>Pattern</i></th>
+                    <th style="background-color: #198754; color: white;"><i>Tesuji</i></th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Cinque</td>
+                    <td>Attacco da casella X<br>Attacco da casella C<br>Attacco da "quattro più uno"</td>
+                </tr>
+                <tr>
+                    <td>Casella C isolate</td>
+                    <td>Attacco alla casella C isolata</td>
+                </tr>
+                <tr>
+                    <td>Formazione doppiamente sbilanciate (quattro più uno - tre più due)</td>
+                    <td>Attacchi alle formazioni doppiamente sbilanciate</td>
+                </tr>
+                <tr>
+                    <td>Quattro sbilanciato, tre sbilanciato, ...</td>
+                    <td>Stoner trap</td>
+                </tr>
+                <tr>
+                    <td>Controllo di linee e di accessi</td>
+                    <td>Swindle</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<h2>Altri <i>pattern</i>? Altri <i>tesuji</i>?</h2>
+
+<p>Oltre a quelli già presentati ci sono moltissimi altri <i>pattern</i>. Molti sono noti e condivisi nella comunità degli
+othellisti, e sono presentati in questo capitolo.</p>
+
+<p>Sono convinto poi che molti giocatori, soprattutto quelli che hanno un approccio teorico al gioco,
+abbiano un proprio catalogo di <i>pattern</i>.</p>
+
+<!-- Se la userai, spiega qui la classificazione dei tesuji, partendo anche dal libro di Rose. -->

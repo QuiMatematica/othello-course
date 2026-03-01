7914ebc9200e67e5ab24b4efb27cc03a1d191861
QuiMatematica <claudio@quimatematica.it>
2026-03-01 19:07:44 +0100

    Pattern e tesuji

---
+++ src/pages/avanzato/pattern1/diagonale.php
@@ -0,0 +1,24 @@
+<div class="d-flex align-items-center text-muted small mt-3">
+  <span>
+    da <strong class="text-dark">Brian Rose, Othello: A Minute to Learn... A Lifetime to Master</strong>
+    &nbsp;
+      <a href="https://www.fngo.it/allegati/brianrose_book(eng).pdf" target="_blank" rel="noopener noreferrer">
+        <i class="bi bi-box-arrow-right"></i>
+    </a>
+  </span>
+</div>
+<div class="d-flex align-items-center text-muted small">
+    <span>
+        traduzione di <strong class="text-dark">Alessandro Di Mattei</strong>
+    </span>
+</div>
+<div class="d-flex align-items-center text-muted small mb-3">
+    <span>
+        adattato da <strong class="text-dark">Claudio Signorini</strong>
+    </span>
+</div>
+
+<p>Il diagramma <span data-board-ref="diagonale-1"></span> mostra un <i>pattern</i> comune di un finale. Potrebbe sembrare
+che il nero abbia perso, ma in realtà ha ancora una possibilità per vincere.</p>
+
+<board data-type="show" data-label="diagonale-1" data-file="diagonale-1.json"></board>

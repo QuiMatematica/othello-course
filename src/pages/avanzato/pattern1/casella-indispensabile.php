aaf8b9240c8be57e87528693b0a1ee07b8dcea50
QuiMatematica <claudio@quimatematica.it>
2026-03-01 18:39:41 +0100

    Pattern e tesuji

---
+++ src/pages/avanzato/pattern1/casella-indispensabile.php
@@ -0,0 +1,32 @@
+<div class="d-flex align-items-center text-muted small mt-3">
+  <span>
+    da <strong class="text-dark">Brian Rose, Othello: A Minute to Learn... A Lifetime to Master</strong>
+    &nbsp;
+      <a href="https://www.fngo.it/allegati/brianrose_book(eng).pdf" target="_blank" rel="noopener noreferrer">
+        <i class="bi bi-box-arrow-right"></i>
+    </a>
+  </span>
+</div>
+<div class="d-flex align-items-center text-muted small">
+    <span>
+        traduzione di <strong class="text-dark">Alessandro Di Mattei</strong>
+    </span>
+</div>
+<div class="d-flex align-items-center text-muted small mb-3">
+    <span>
+        adattato da <strong class="text-dark">Claudio Signorini</strong>
+    </span>
+</div>
+
+<p>Nel diagramma <span data-board-ref="casella-indispensabile-1"></span> il nero ha un disperato bisogno di accedere
+alla casella <b>H1</b> e può ottenerlo giocando in <b>C8</b>, portando un attacco all'angolo in <b>A8</b>.</p>
+
+<board data-type="show" data-label="casella-indispensabile-1" data-file="casella-indispensabile-1.json"></board>
+
+<p>Si noti come, modificando il diagramma spostando la pedina <b>B8</b> in <b>H7</b>
+    (diagramma <span data-board-ref="casella-indispensabile-2"></span>), la mossa in <b>C8</b> non
+è altrettanto efficace.</p>
+
+<board data-type="show" data-label="casella-indispensabile-2" data-file="casella-indispensabile-2.json"></board>
+
+<p>Era il fatto che il nero stesse attaccando un angolo che garantisce l'accesso in <b>H1</b>.</p>
---
+++ src/pages/avanzato/pattern1/pedine-avvelenate.php
@@ -0,0 +1,24 @@
+<div class="d-flex align-items-center text-muted small mt-3">
+  <span>
+    da <strong class="text-dark">Brian Rose, Othello: A Minute to Learn... A Lifetime to Master</strong>
+    &nbsp;
+      <a href="https://www.fngo.it/allegati/brianrose_book(eng).pdf" target="_blank" rel="noopener noreferrer">
+        <i class="bi bi-box-arrow-right"></i>
+    </a>
+  </span>
+</div>
+<div class="d-flex align-items-center text-muted small">
+    <span>
+        traduzione di <strong class="text-dark">Alessandro Di Mattei</strong>
+    </span>
+</div>
+<div class="d-flex align-items-center text-muted small mb-3">
+    <span>
+        adattato da <strong class="text-dark">Claudio Signorini</strong>
+    </span>
+</div>
+
+<p>Il diagramma <span data-board-ref="pedine-avvelenate-1"></span> mostra un esempio tipici di questo <i>tesuji</i>.
+</p>
+
+<board data-type="show" data-label="pedine-avvelenate-1" data-file="pedine-avvelenate-1.json"></board>

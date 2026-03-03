f327daf40ef87a6d7b5e17f109ca1ee633a84687
QuiMatematica <claudio@quimatematica.it>
2026-03-03 20:03:46 +0100

    Pattern e tesuji

---
+++ src/pages/avanzato/pattern1/doppio-attacco.php
@@ -0,0 +1,42 @@
+<div class="d-flex align-items-center text-muted small mt-3">
+  <span>
+    da <strong class="text-dark">Brian Rose, Othello: A Minute to Learn... A Lifetime to Master</strong>
+    &nbsp;
+      <a href="https://www.fngo.it/allegati/brianrose_book(eng).pdf" target="_blank" rel="noopener noreferrer">
+        <i class="bi bi-box-arrow-right"></i>
+    </a>
+  </span>
+</div>
+<div class="d-flex align-items-center text-muted small">
+    <span>
+        traduzione di <strong class="text-dark">Alessandro Di Mattei</strong>
+    </span>
+</div>
+<div class="d-flex align-items-center text-muted small mb-3">
+    <span>
+        adattato da <strong class="text-dark">Claudio Signorini</strong>
+    </span>
+</div>
+
+<p>Il diagramma <span data-board-ref="doppio-attacco-1"></span> mostra un pattern che si presenta spesso
+sia in centro partita che nei finali.</p>
+
+<board data-type="show" data-label="doppio-attacco-1" data-file="doppio-attacco-1.json"></board>
+
+<p>Ho visto molti giocatori, in questa situazione, prendere direttamente l'angolo, per il timore di perderne
+l'accesso. Il problema di muovere in <b>A8</b> è che il bianco potrà incunearsi in <b>E8</b> e il nero
+resterà a corto di mosse (diagramma <span data-board-ref="doppio-attacco-2"></span>).</p>
+
+<board data-type="show" data-label="doppio-attacco-2" data-file="doppio-attacco-2.json"></board>
+
+<p>&Egrave; molto meglio per il nero giocare egli stesso in <b>E8</b>, al fine di creare un doppio attacco
+all'angolo (diagramma <span data-board-ref="doppio-attacco-3"></span>).</p>
+
+<board data-type="show" data-label="doppio-attacco-3" data-file="doppio-attacco-3.json"></board>
+
+<p>Poiché un doppio attacco all'angolo permette quasi sempre di conquistare l'angolo, se questo è piuttosto utile
+non dovresti esitare a sacrificare un bordo. Nel diagramma <span data-board-ref="doppio-attacco-4"></span> potresti
+essere tentato a giocare <b>G7</b>, <b>H7</b> o qualche altra mossa del genere, ma muovendo in <b>E8</b>
+può ottenere una facile vittoria.</p>
+
+<board data-type="show" data-label="doppio-attacco-4" data-file="doppio-attacco-4.json"></board>

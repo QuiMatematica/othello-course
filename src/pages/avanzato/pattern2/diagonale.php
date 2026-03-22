42f1413f6f2cabe01071b8d10f6bb677ccaa27de
QuiMatematica <claudio@quimatematica.it>
2026-03-22 19:32:24 +0100

    Cron cleanup

---
+++ src/pages/avanzato/pattern2/diagonale.php
@@ -0,0 +1,13 @@
+<p>La struttura per applicare questo <i>tesuji</i> appare molto presto nel gioco, a volte anche prima del centro
+partita.</p>
+
+<p>Nel diagramma <span data-board-ref="diagonale-1"></span> il nero ha sacrificato l'angolo in <b>H8</b>
+    per guadagnare un tempo.</p>
+
+<board data-type="show" data-label="diagonale-1" data-file="diagonale-1.json"></board>
+
+<p>Un punto molto importante da tener presente è che lo <i>swindle</i> è stato possibile solo nel momento
+    in cui il nero ha tagliato la diagonale. Ad esempio, se il nero avesse giocato <b>A4</b> allora
+il bianco non avrebbe potuto rispondere in <b>G1</b> (diagramma <span data-board-ref="diagonale-2"></span>).</p>
+
+<board data-type="show" data-label="diagonale-2" data-file="diagonale-2.json"></board>

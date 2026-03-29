fee5f7f275b6e1b6a5d447689a9bf878ce8e7e6c
QuiMatematica <claudio@quimatematica.it>
2026-03-29 19:13:53 +0200

    Pattern 2

---
+++ src/pages/avanzato/pattern2/tesuji-e-angoli.php
@@ -0,0 +1,18 @@
+<p>In questa seconda raccolta di <i>tesuji</i> ti presento diverse situazioni che riguardano regioni d'angolo.</p>
+
+<p>Ogni regione chiusa, infatti, va analizzata per capire se se ne può sfruttare le caratteristiche, in modo da
+guadagnare tempi di giochi e parità.</p>
+
+<p>Il tema di intreccia fortemente con quello degli <i>swindle</i>. Ma questo non deve stupirti: di fatto ogni
+<i>swindle</i> è un <i>tesuji</i>, come giustamente sottolinea Brian Rose nel suo libro
+    <a href="https://www.fngo.it/allegati/brianrose_book(eng).pdf" target="_blank" rel="noopener noreferrer">
+        <i class="bi bi-box-arrow-right"></i>
+    </a>.</p>
+
+<p>Inizieremo con l'analizzare regioni formate da quattro caselle. Sappiamo già che una regione siffatta è svantaggiosa
+per chi ci muove per primo... a meno che non ci sia uno <i>swindle</i> a disposizione.</p>
+
+<p>Vedremo poi alcune possibilità interessanti per le regioni formate da cinque caselle.</p>
+
+<p>Chiudono il capitolo alcune situazioni in cui avremo la possibilità di procurarci uno <i>swindle</i> o di salvare
+un bordo malgrado la perdita dei due angoli adiacenti.</p>

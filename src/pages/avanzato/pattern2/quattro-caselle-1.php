56e54f3eeb4c04578000e292a5c13ae18ab1716e
QuiMatematica <claudio@quimatematica.it>
2026-03-29 16:54:21 +0200

    Pattern 2

---
+++ src/pages/avanzato/pattern2/quattro-caselle-1.php
@@ -0,0 +1,11 @@
+<p>Una regione d'angolo formata da quattro caselle può essere oggetto di più di uno <i>swindle</i>.</p>
+
+<p>Il caso mostrato in questa pagina è reso possibile dalla presenza di due bordi pieni come, per esempio,
+due quattro pieni.</p>
+
+<board data-type="show" data-label="quattro-caselle-1-1" data-file="quattro-caselle-1-1.json"></board>
+
+<p>L'esempio mostrato sfrutta solamente la regione per ottenere tre mosse su quattro disponibili. Ma osserva cosa
+succede se i due bordi sono dei cinque completi.</p>
+
+<board data-type="show" data-label="quattro-caselle-1-2" data-file="quattro-caselle-1-2.json"></board>

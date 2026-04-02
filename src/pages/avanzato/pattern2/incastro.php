1a58723890fa51ebee7cb81b6b6b89e5c1b97c17
QuiMatematica <claudio@quimatematica.it>
2026-04-02 19:38:14 +0200

    Pattern 2

--- src/pages/avanzato/pattern2/incastro.php
+++ src/pages/avanzato/pattern2/incastro.php
@@ -1,1 +1,16 @@
+<p>Nella posizione del diagramma <span data-board-ref="incastro-1"></span>, il nero vorrebbe salvare il bordo sud.
+Ma la diagonale C <b>C7-H2</b> è controllata dal bianco, per cui il nero non può giocare in <b>B8</b>.
+Ma c'è un'altra possibilità, resa possibile dalla regione dispari a sud-ovest.</p>
+
 <board data-type="show" data-label="incastro-1" data-file="incastro-1.json"></board>
+
+<h2>Il <i>pattern</i></h2>
+
+<p>Il <i>pattern</i> (diagramma <span data-board-ref="incastro-2"></span>) che permette questo <i>tesuji</i>
+    prevede la presenza di:</p>
+<ul>
+    <li>una regione pari in cui cedere il primo angolo (riquadro rosso;</li>
+    <li>una regione dispari in cui guadagnare il tempo di gioco (riquadro giallo).</li>
+</ul>
+
+<board data-type="show" data-label="incastro-2" data-file="incastro-2.json"></board>
--- src/pages/avanzato/pattern2/sacrificare-la-diagonale.php
+++ src/pages/avanzato/pattern2/sacrificare-la-diagonale.php
@@ -1,1 +1,16 @@
+<p>Talvolta può essere utile sacrificare una diagonale o un bordo per poter giocare uno <i>swindle</i>.
+Il vantaggio derivante dal guadagna del tempo di gioco, magari unito a quello della parità, compensa
+opportunamente il sacrificio.</p>
+
 <board data-type="show" data-label="sacrificare-la-diagonale-1" data-file="sacrificare-la-diagonale-1.json"></board>
+
+<h2>Il <i>pattern</i></h2>
+
+<p>Il <i>pattern</i> (diagramma <span data-board-ref="sacrificare-la-diagonale-2"></span>) che permette questo <i>tesuji</i>
+    prevede la presenza di:</p>
+<ul>
+    <li>la possibilità di sacrificare la diagonale facendo in modo che l'avversario ne ottenga il controllo (freccia gialla);</li>
+    <li>la possibilità di muovere due mosse in una stessa regione grazie al controllo della diagonale (frecce viola).</li>
+</ul>
+
+<board data-type="show" data-label="sacrificare-la-diagonale-2" data-file="sacrificare-la-diagonale-2.json"></board>

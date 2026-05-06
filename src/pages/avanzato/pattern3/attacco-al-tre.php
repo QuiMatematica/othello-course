51cd486bb3f9d04c89bb4640ab2f39b46e1a4fa0
QuiMatematica <claudio@quimatematica.it>
2026-05-06 18:48:38 +0200

    Mangiare

---
+++ src/pages/avanzato/pattern3/attacco-al-tre.php
@@ -0,0 +1,10 @@
+<p>Talvolta si tende a sottovalutare la pericolosità di un bordo sbilanciato e in particolare di un
+<i>tre sbilanciato</i>.</p>
+
+<p>In particolari situazioni, con il controllo delle giuste linee, un tre sbilanciato può essere attaccato in modo
+    simile a quanto abbiamo già visto per le
+    <a href="../../intermedio/altri-attacchi-ai-bordi/attacchi-alle-caselle-c.php">caselle C isolate</a>.
+    Questo tipo di attacco garantisce l'accesso all'angolo
+e il controllo di buona parte del bordo.</p>
+
+<board data-type="show" data-label="attacco-al-tre-1" data-file="attacco-al-tre-1.json"></board>

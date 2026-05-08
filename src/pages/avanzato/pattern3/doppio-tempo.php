89a0cc77808b2d7bc946d14bc4d20b5b0f7425e3
QuiMatematica <claudio@quimatematica.it>
2026-05-08 17:15:43 +0200

    Doppio tempo sul bordo

---
+++ src/pages/avanzato/pattern3/doppio-tempo.php
@@ -0,0 +1,17 @@
+<p>La situazione presentata nel diagramma <span data-board-ref="doppio-tempo-1"></span> è piuttosto rara.
+Ma se l'opportunità si presenta, va sicuramente colta. Il guadagno di due tempi di gioco e la presa di un sei
+sul bordo è sicuramente un vantaggio molto forte.</p>
+
+<board data-type="show" data-label="doppio-tempo-1" data-file="doppio-tempo-1.json"></board>
+
+<h2>Il <i>pattern</i></h2>
+
+<p>Il <i>pattern</i> (diagramma <span data-board-ref="doppio-tempo-2"></span>) che permette questo <i>tesuji</i>
+    prevede la presenza di:</p>
+<ul>
+    <li>un bordo da riempire (riquadro rosso);</li>
+    <li>la possibilità per l'attaccante di giocare nella casella <b>C</b> (freccia gialla);</li>
+    <li>l'impossibilità per l'attaccato di incunearsi (freccia viola).</li>
+</ul>
+
+<board data-type="show" data-label="doppio-tempo-2" data-file="doppio-tempo-2.json"></board>

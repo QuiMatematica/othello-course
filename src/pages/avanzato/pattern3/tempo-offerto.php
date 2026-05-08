f65ce3a55d3a79e19afb0424539df7a2ec67be40
QuiMatematica <claudio@quimatematica.it>
2026-05-08 18:53:34 +0200

    Tempo offerto

---
+++ src/pages/avanzato/pattern3/tempo-offerto.php
@@ -0,0 +1,3 @@
+<p>Questo <i>tesuji</i> è estremamente semplice, tuttavia spesso sono le cose più semplici che ci sfuggono.</p>
+
+<board data-type="show" data-label="tempo-offerto-1" data-file="tempo-offerto-1.json"></board>

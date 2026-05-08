09e7b101f4b4f6a34704cbab03cf6fbe083207bd
QuiMatematica <claudio@quimatematica.it>
2026-05-08 14:37:33 +0200

    Attacco al tre sbilanciato

--- src/pages/avanzato/pattern3/attacco-al-tre.php
+++ src/pages/avanzato/pattern3/attacco-al-tre.php
@@ -5,6 +5,18 @@
     simile a quanto abbiamo già visto per le
     <a href="../../intermedio/altri-attacchi-ai-bordi/attacchi-alle-caselle-c.php">caselle C isolate</a>.
     Questo tipo di attacco garantisce l'accesso all'angolo
-e il controllo di buona parte del bordo.</p>
+e il controllo di buona parte del bordo. Inoltre il giocatore attaccante guadagna due tempi di gioco.</p>
 
 <board data-type="show" data-label="attacco-al-tre-1" data-file="attacco-al-tre-1.json"></board>
+
+<h2>Il <i>pattern</i></h2>
+
+<p>Il <i>pattern</i> (diagramma <span data-board-ref="attacco-al-tre-2"></span>) che permette questo <i>tesuji</i>
+    prevede la presenza di:</p>
+<ul>
+    <li>un tre sbilanciato da attaccare (riquadro rosso);</li>
+    <li>la possibilità per l'attaccante di giocare nella casella <b>C</b> opposta (freccia gialla);</li>
+    <li>l'impossibilità per l'attaccato di rispondere nelle due caselle libere rimaste (frecce viola).</li>
+</ul>
+
+<board data-type="show" data-label="attacco-al-tre-2" data-file="attacco-al-tre-2.json"></board>

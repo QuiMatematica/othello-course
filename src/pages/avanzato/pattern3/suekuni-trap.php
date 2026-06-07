4611e13630af0916910b8f9376c97d6794e56c89
QuiMatematica <claudio@quimatematica.it>
2026-06-07 19:29:13 +0200

    Suekuni trap

---
+++ src/pages/avanzato/pattern3/suekuni-trap.php
@@ -0,0 +1,41 @@
+<p>Stoccolma, Campionato Mondiale del 2003. Al decimo turno di gioco si incontrano i due giocatori più forti
+del momento: il giapponese Makoto Suekuni con il nero e lo statunitense Ben Seeley con il bianco.</p>
+
+<p>Spoiler: Suekuni e Seeley saranno i finalisti di quel mondiale, vinto dall'americano.</p>
+
+<p>Il diagramma <span data-board-ref="suekuni-trap-1"></span> mostra la partita dalla mossa 35 alla mossa 38.</p>
+
+<board data-type="show" data-label="suekuni-trap-1" data-file="suekuni-trap-1.json"></board>
+
+<p>Analizziamo per bene gli effetti dell'ultima mossa giocata da Ben Seeley.</p>
+
+<p>Giocando in <b>H4</b> Seeley ha voltato la pedina <b>G3</b> (diagramma <span data-board-ref="suekuni-trap-2"></span>).</p>
+
+<board data-type="show" data-label="suekuni-trap-2" data-file="suekuni-trap-2.json"></board>
+
+<p>Girando la pedina <b>G3</b>, si forma una regione formata da cinque caselle. L'elemento interessante è che l'unica
+mossa a disposizione del bianco è <b>H2</b>, che ovviamente offre la scontata risposta in <b>H1</b>
+    (diagramma <span data-board-ref="suekuni-trap-3"></span>).</p>
+
+<board data-type="show" data-label="suekuni-trap-3" data-file="suekuni-trap-3.json"></board>
+
+<p>Ora... facciamo un po' di conti per quanto riguarda la <a href="../../intermedio/parita/chapter.php">parità</a>.
+&Egrave; il turno del nero. Nessuno dei due giocatori ha passato. Quindi c'è un numero <i>pari</i> di caselle vuote.
+Per la precisione, visto che è appena stata giocata la mosa 38, ci sono 22 caselle vuote.</p>
+
+<p>Il bianco ha un enorme svantaggio a giocare nella regione a nord-est. Quindi farà di tutto per giocare altrove.</p>
+
+<p>Se togliamo le cinque caselle della regione a nord-est, rimane un numero <i>dispari</i> di caselle vuote. Per la
+precisione 17. Quindi il nero ha la possibilità di giocare l'ultima mossa di queste 17 disponibili.</p>
+
+<p>Una volta riempite le 17 caselle vuote, al bianco rimarrà una sola mossa: proprio <b>H2</b>.</p>
+
+<p>Il nero potrà rispondere in <b>H1</b>... e rimane una regione di tre caselle in cui il bianco non ha accesso.
+Il nero ha ottenuto la <i>parità</i>!</p>
+
+<p>Nel diagramma <span data-board-ref="suekuni-trap-4"></span> puoi seguire il resto della partita con qualche commento.</p>
+
+<board data-type="show" data-label="suekuni-trap-4" data-file="suekuni-trap-4.json"></board>
+
+<p>Alla fine di questa partita Ben Seeley ha battezzato <b>Suekuni trap</b> questo <i>pattern</i>. Suekuni in verità
+non ha mai desiderato questa paternità, ma ormai il nome è assegnato.</p>

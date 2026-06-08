516c3a6d8ac28970ac7a6fe0039c20405760747e
QuiMatematica <claudio@quimatematica.it>
2026-06-08 15:39:25 +0200

    Suekuni trap

--- src/pages/avanzato/pattern3/suekuni-trap.php
+++ src/pages/avanzato/pattern3/suekuni-trap.php
@@ -39,3 +39,46 @@ Il nero ha ottenuto la <i>parità</i>!</p>
 
 <p>Alla fine di questa partita Ben Seeley ha battezzato <b>Suekuni trap</b> questo <i>pattern</i>. Suekuni in verità
 non ha mai desiderato questa paternità, ma ormai il nome è assegnato.</p>
+
+<h2>Il <i>pattern</i></h2>
+
+<p>Il <i>pattern</i> (diagramma <span data-board-ref="suekuni-trap-pattern"></span>) che permette questo <i>tesuji</i>
+    prevede la presenza di:</p>
+<ul>
+    <li>il caratteristico bordo con struttura a incastro (riquadro giallo);</li>
+    <li>una regione adiacente dispari in cui l'ultima mossa del bianco risulta essere la casella <b>C</b> che andrà a
+    cedere l'angolo e far perdere una mossa (caselle rosse);</li>
+    <li>affinché l'unica mossa del bianco sia la casella <b>C</b>, è importante anche in controbordo, in particolare
+        la pedine affacciata sulla regione deve essere bianca (riquadro blu).</li>
+</ul>
+
+<board data-type="show" data-label="suekuni-trap-pattern" data-file="suekuni-trap-pattern.json"></board>
+
+<p>Ovviamente la struttura <i>a incastro</i> potrebbe anche essere diversa. Ma soprattutto deve rimanere fino alla
+fine della partita, quando finalmente si sfrutteranno le caratteristiche dalla posizione. Per esempio, rimanendo
+sempre nella partita analizzata, anche a mossa 55 è presente la struttura, anche se è stata modificata dalla mosse
+giocate (riquadro giallo nel diagramma <span data-board-ref="suekuni-trap-pattern-finale"></span>).</p>
+
+<board data-type="show" data-label="suekuni-trap-pattern-finale" data-file="suekuni-trap-pattern-finale.json"></board>
+
+<p>Un ultimo elemento da osservare è che tale <i>pattern</i>, per come coinvolge la parità, è più efficace
+se giocato dal nero.</p>
+
+<p>Se fosse giocato dal bianco, infatti, potrebbero succedere due cose.</p>
+<ul>
+    <li>Se la regione adiacente fosse dispari e venisse lasciata per ultima, a meno di altre forzatura in altre regioni,
+        sarà il bianco a doverci
+    giocare per primo, rendendo di fatto inutile la trappola (vedi diagramma <span data-board-ref="suekuni-trap-5"></span>).</li>
+</ul>
+
+<board data-type="show" data-label="suekuni-trap-5" data-file="suekuni-trap-5.json"></board>
+
+<ul>
+    <li>Se la regione adiacente fosse pari, sempre a meno di altre forzature in altre regioni, sarà il nero a doverci
+    giocare per primo e dovrà farlo nella casella <b>C</b>. Ma a questo punto se il bianco prende l'angolo, poi il nero
+    passa e ottiene l'ultima mossa (vedi diagramma <span data-board-ref="suekuni-trap-6"></span>).</li>
+</ul>
+
+<board data-type="show" data-label="suekuni-trap-6" data-file="suekuni-trap-6.json"></board>
+
+<p>Quindi giocato dal bianco il vantaggio è sicuramente minore.</p>

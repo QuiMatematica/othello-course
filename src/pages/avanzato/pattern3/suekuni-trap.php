12da08fc1818e8535d078233eda8974c53fb9a60
QuiMatematica <claudio@quimatematica.it>
2026-06-08 17:44:37 +0200

    Suekuni trap

--- src/pages/avanzato/pattern3/suekuni-trap.php
+++ src/pages/avanzato/pattern3/suekuni-trap.php
@@ -48,7 +48,7 @@ non ha mai desiderato questa paternità, ma ormai il nome è assegnato.</p>
     <li>il caratteristico bordo con struttura a incastro (riquadro giallo);</li>
     <li>una regione adiacente dispari in cui l'ultima mossa del bianco risulta essere la casella <b>C</b> che andrà a
     cedere l'angolo e far perdere una mossa (caselle rosse);</li>
-    <li>affinché l'unica mossa del bianco sia la casella <b>C</b>, è importante anche in controbordo, in particolare
+    <li>affinché l'unica mossa del bianco sia la casella <b>C</b>, è importante anche il controbordo, in particolare
         la pedine affacciata sulla regione deve essere bianca (riquadro blu).</li>
 </ul>
 
@@ -56,7 +56,7 @@ non ha mai desiderato questa paternità, ma ormai il nome è assegnato.</p>
 
 <p>Ovviamente la struttura <i>a incastro</i> potrebbe anche essere diversa. Ma soprattutto deve rimanere fino alla
 fine della partita, quando finalmente si sfrutteranno le caratteristiche dalla posizione. Per esempio, rimanendo
-sempre nella partita analizzata, anche a mossa 55 è presente la struttura, anche se è stata modificata dalla mosse
+sempre nella partita analizzata, anche alla mossa 55 è presente la struttura, sebbene sia stata modificata dalla mosse
 giocate (riquadro giallo nel diagramma <span data-board-ref="suekuni-trap-pattern-finale"></span>).</p>
 
 <board data-type="show" data-label="suekuni-trap-pattern-finale" data-file="suekuni-trap-pattern-finale.json"></board>
@@ -74,7 +74,8 @@ se giocato dal nero.</p>
 <board data-type="show" data-label="suekuni-trap-5" data-file="suekuni-trap-5.json"></board>
 
 <ul>
-    <li>Se la regione adiacente fosse pari, sempre a meno di altre forzature in altre regioni, sarà il nero a doverci
+    <li>Se la regione adiacente fosse pari e venisse lasciata per ultima, sempre a meno di altre forzature in altre
+        regioni, sarà il nero a doverci
     giocare per primo e dovrà farlo nella casella <b>C</b>. Ma a questo punto se il bianco prende l'angolo, poi il nero
     passa e ottiene l'ultima mossa (vedi diagramma <span data-board-ref="suekuni-trap-6"></span>).</li>
 </ul>
@@ -82,3 +83,30 @@ se giocato dal nero.</p>
 <board data-type="show" data-label="suekuni-trap-6" data-file="suekuni-trap-6.json"></board>
 
 <p>Quindi giocato dal bianco il vantaggio è sicuramente minore.</p>
+
+<h2>Disinnescare una Suekuni trap</h2>
+
+<p>Se la struttura del bordo a incastro è fondamentale per la riuscita della Suekuni trap, può capitare che cedere
+anzitempo l'angolo opposto alla regione minacciata sia vantaggioso e si ottenga così di <i>disinnescare</i>
+la trappola.</p>
+
+<p>Ne è un esempio la posizione del diagramma <span data-board-ref="suekuni-trap-7"></span>, ottenuta modificando
+la struttura del bordo sud del diagramma <span data-board-ref="suekuni-trap-1"></span>.</p>
+
+<board data-type="show" data-label="suekuni-trap-7" data-file="suekuni-trap-7.json"></board>
+
+<h2>Un altro esempio</h2>
+
+<p>Il diagramma <span data-board-ref="suekuni-trap-8"></span> è presentato da Tetsuya Nakajima in un video
+proprio sulle Suekuni trap <a href="https://www.youtube.com/watch?v=wUN4jbnUqDY" target="_blank" rel="noopener noreferrer">
+        <i class="bi bi-box-arrow-right"></i>
+    </a>. Il video ha l'audio in giapponese, ma con i sottotitoli automatici si riesce comunque
+a seguire la spiegazione. Interessante che chiami <i>bomba</i> la struttura a incastro sul bordo.</p>
+
+<board data-type="show" data-label="suekuni-trap-8" data-file="suekuni-trap-8.json"></board>
+
+<h2>Tocca a te</h2>
+
+<p>Questo esempio è stato proposto dal maestro Paolo Scognamiglio sul gruppo WhatsApp degli othellisti italiani.</p>
+
+<board data-type="quiz" data-label="suekuni-trap-9" data-file="suekuni-trap-9.json"></board>

79f0fd3efc2fd3469164349b5f476df6c64f8390
QuiMatematica <claudio@quimatematica.it>
2026-07-03 20:02:20 +0200

    Gioco chiuso

---
+++ src/pages/avanzato/gioco-chiuso/camminare-sui-bordi.php
@@ -0,0 +1,13 @@
+<p>Se sei stato bravo ad accumulare un discreto vantggio in apertura, giocare il finale potrebbe risultare molto
+    facile.</p>
+
+<p>Se il tuo avversario ha finito le mosse sicure, ti basterà, una volta conquistato un angolo,
+    <b>camminare lungo i bordi</b> adiacenti all'angolo appena conquistato, come mostrato nel diagramma
+<span data-board-ref="camminare-sui-bordi-1"></span></p>
+
+<board data-type="show" data-label="camminare-sui-bordi-1" data-file="camminare-sui-bordi-1.json"></board>
+
+<p>Ora, se proviamo a schematizzare la sequenza delle mosse del nero, abbiamo la situazione mostrata nel diagramma
+<span data-board-ref="camminare-sui-bordi-2"></span></p>
+
+<board data-type="show" data-label="camminare-sui-bordi-2" data-file="camminare-sui-bordi-2.json"></board>

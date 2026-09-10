d7583c407f7a95d62b02be081c1896f01a094943
QuiMatematica <claudio@quimatematica.it>
2026-09-10 21:33:13 +0200

    Xot by CAffa

---
+++ src/pages/avanzato/approfondimenti/xot.php
@@ -0,0 +1,108 @@
+<p>C’è una nuova moda che sta prendendo piede nella nostra comunità. L’Othello XOT, la versione
+alternativa del gioco con le aperture casuali.</p>
+<p>Per chi non ci avesse ancora avuto a che fare: invece di sederti davanti all’Othelliera, far partire il gioco e
+iniziare con le mosse delle vostre aperture preferite, nell’Othello XOT le prime otto mosse sono già
+giocate. La partita inizia in una posizione estratta da una lunga lista di aperture codificate in modo tale
+che non ci sia troppa differenza tra i due giocatori in termini di valutazione algoritmica, e da lì il gioco
+inizia come sempre, col nero che ha la prima mossa.</p>
+
+<board data-type="show" data-label="xot1" data-file="xot1.json"
+       data-caption="Una apertura XOT, tra quelle emerse campionato europeo 2026."></board>
+
+<p>Si tratta di un modo per rendere il gioco più dinamico e imprevedibile. Non si può fare affidamento sul
+nostro book delle aperture, non si può più contare sul fatto che giocheremo le prime mosse a memoria,
+senza bisogno di riflettere. Si ragiona pesantemente fin dalla prima mossa, spesso anche più che con
+l’Othello standard. Questo perché le posizioni hanno molto spesso una certa follia latente, e si finisce per
+giocare in direzioni che normalmente non considereremmo naturali. Con l’Othello normale le eviteremmo
+scegliendo aperture e sequenze differenti, ma nello XOT spesso è la posizione iniziale che ti costringe in
+giocate più scomode del solito.</p>
+<p>Da un lato, quindi, Othello diventa più eccitante. Niente più lunghe draw sequences note, niente più
+partite giocate per gran parte a memoria. Dall’altro, si perde più facilmente, anche se sei un top player. In
+un torneo XOT (vedi
+    <a href="https://www.worldothello.org/about/tournaments/confederate-championships/european-othello-tournaments/european-xot-championship" target="_blank" rel="noopener noreferrer">il
+        primo campionato Europeo di settembre 2026</a>), la classifica è più dinamica, c’è un
+fattore casuale più marcato che rimescola le carte, e anche le posizioni in partita diventano più
+spettacolari: spesso non si tratta di trovare la mossa analiticamente più naturale o sicura, ma quella
+capace di navigare il caos in maniera più efficace.</p>
+
+<h2>I tornei XOT: una sfida di abilità e visione</h2>
+<p>In un torneo di Othello normale, tipicamente i giocatori hanno alcune armi a proprio vantaggio per riuscire
+bene. Le abilità dell’Othello stanno alla base, ovviamente, ma accanto a queste c’è la possibilità di
+effettuare una preparazione particolare per affinare alcune altre armi, che possono permetterti di riuscire
+meglio.</p>
+
+<p>La questione emersa recentemente riguardo
+    <a href="https://www.worldothello.org/news/445/the-catch-22-perfect-storm" target="_blank" rel="noopener noreferrer">la
+        memorizzazione di lunghe sequenze perfette</a> è solo una
+di quelle armi. Usare la memoria per ridurre la necessità di ragionamento, per risparmiare tempo, e per
+reggere le dinamiche di gioco a cui alcuni top players ti costringono, è una delle componenti che si
+possono preparare con ore di studio prima di un torneo importante. Non richiede abilità Othello in senso
+stretto, ma soprattutto memoria, e può darti un vantaggio oggettivo, ovviamente. Per alcuni fa parte del
+gioco, per altri può renderlo noioso da vedere dall’esterno, qui non affronteremo la questione.</p>
+<p>Altri modi di prepararsi a un torneo sono studiare alcuni giocatori specifici (magari provando a condurli su
+un territorio ben preciso in base alle aperture che solitamente giocano) o preparare aperture alternative
+che possano sorprendere gli avversari. Messo tutto in conto, abbiamo una conclusione ovvia: un
+giocatore può avere una prestazione migliore a un torneo se spende ore ogni giorno a prepararsi per
+quella competizione. Più lunga è la preparazione, più grossi i potenziali vantaggi che se ne possono
+ottenere rispetto a uno che si è preparato meno, o che non si prepara affatto. Da questo punto di vista,
+possiamo dire che un torneo di Othello standard premia il giocatore con la più alta somma di abilità
+naturali + preparazione. E se ci pensiamo bene, ha perfettamente senso: in ogni sport al mondo il
+successo di un atleta dipende non solo dal suo talento naturale ma anche dallo sforzo che ci ha messo
+ad allenarsi prima di una specifica competizione, ed è giusto che quello sforzo vada premiato.</p>
+<p>Con l’Othello XOT, invece, la preparazione non può esserci (partiamo dal presupposto che l’improbabile
+intenzione di memorizzare tutte le aperture XOT esistenti e studiarne le continuazioni sia un’impresa
+inumana e complessivamente inefficace in termini di rapporto sforzo/risultato). Senza la preparazione,
+Othello XOT diventa tendenzialmente una sfida di pure abilità, puro talento naturale. Il che è affascinante
+da un punto di vista strettamente teorico, ed è comprensibile che alcuni abbiano iniziato ad amarlo,
+accanto ad altri che preferiscono ancora avere quegli altri elementi a contorno che diano più controllo
+alla cosa.</p>
+<p>Abbiamo trovato la soluzione perfetta per il futuro dell’Othello allora? Si e no, in realtà. Perché accanto al
+piacere di una competizione fatta di sole abilità c’è un aspetto nascosto insidioso...</p>
+
+<h2>Il peso del caos</h2>
+<p>Othello XOT ha una componente casuale molto più pesante. Il caos sull’Othelliera non è l’eccezione, è la
+regola. E il caos genera ovviamente situazioni più imprevedibili, che possono portare a una sconfitta
+anche se hai messo in gioco tutte le Othello-skills alla perfezione. Il che spiega perché al primo europeo
+XOT abbiamo assistito a punteggi mediamente più bassi rispetto ai tornei normali. Si perde
+generalmente di più, insomma. E non è qualcosa che puoi prevenire con maggiore preparazione o ore di
+studio.</p>
+<p>Giocare in modalità XOT ti mette di fronte spessissimo a situazioni scomode. Prendere un bordo molto
+presto, giocare fuori dagli “sweet sixteen” in anticipo, avere una X-square già occupata o essere costretti
+a giocarla prestissimo... sono tutte cose che possono accadere anche nell’Othello normale, ma di solito lì
+abbiamo libertà di scelta. Possiamo scegliere continuazioni ancora accettabili in direzioni più standard, o
+possiamo scegliere di cambiare apertura prima, per evitare di finire nelle posizioni in cui non abbiamo
+confidenza.</p>
+<p>Nel mondo XOT, invece, spesso l’alternativa non c’è, o c’è ma porta a uno svantaggio ancora peggiore.
+Capita spesso che ci sia una sola mossa che ci faccia rimanere a galla, e se ci capita di non vederla o di
+non volerla perché porta a posizioni troppo assurde, si rischia che la partita sia finita con largo anticipo.</p>
+
+<board data-type="show" data-label="xot2" data-file="xot2.json"
+       data-caption="Morte a mossa 28."></board>
+Morte a mossa 28: da Christian Sommer vs. Carlo Affatigato, XOT EOC 2026
+
+<p><b>Questo caos bisogna navigarlo</b>, insomma. E in quanto caos, l’entropia è maggiore, e la possibilità di
+errore più alta. È come se avessimo improvvisamente introdotto una maggiore componente casuale,
+quasi come aggiungere un lancio di dadi a inizio partita. Resta ancora un gioco pienamente
+deterministico, il cui risultato dipende solo e soltanto dalle scelte dei giocatori. Ma la fortuna/sfortuna
+entra in gioco se arrivano delle posizioni in cui la mossa giusta è talmente assurda o irragionevole che
+praticamente nessuno al mondo (umani, intendo) la vedrebbe. Se capita, bisogna accettare che si perde
+perché siamo stati abbastanza sfortunati da beccare una posizione troppo difficile. E questo non vale
+solo per le prime otto mosse: l’effetto può propagarsi e diventare esplicito anche a metà partita o
+addirittura nelle parti finali.</p>
+<p>Questo ovviamente può piacere o non piacere. Molti di noi giocano a Othello invece che a yahtzee o
+poker perché amano l’aspetto deterministico del gioco, e magari non prendono con entusiasmo
+l’introduzione di un maggiore aspetto caotico. Altri invece lo accolgono a braccia aperte perché il puro
+controllo di ogni fattore (incluso quello mnemonico che si appoggia sull’aiuto dei software in fase di
+preparazione) sta uccidendo il divertimento del gioco. È l’altro volto della medaglia, e vale la pena tenerlo
+presente se deciderete di partecipare più spesso in futuro a tornei XOT.</p>
+<p>Personalmente, all’europeo XOT io mi sono divertito un sacco. Alcuni di voi già lo sanno,
+    <a href="aperture2.php">cavalcare il caos è diventato il mio modus operandi</a>,
+    e infatti sento che il mondo XOT in qualche modo sia più
+compatibile di quello standard al modo in cui mi piace giocare. Come me tanti altri, mentre dall’altra parte
+ne esistono altrettanti che preferiscono mantenersi su lidi sicuri. La risposta giusta non c’è, ed è
+auspicabile che le due strade continuino a svilupparsi in parallelo, in modo da offrire tutte le possibilità a
+partecipanti e spettatori.</p>
+<p>Noi comunque vi invitiamo a buttarvi. Abbandonare i timori e lasciarsi andare a questo caos
+imprevedibile, alle combinazioni spettacolari, alle posizioni improbabili, ai cambi di rotta improvvisi, ai
+recuperi mozzafiato, agli erroracci che ti lasciano a bocca aperta... Othello aveva bisogno di una scossa,
+e ora che è arrivata, potrebbe diventare la nostra nuova dose periodica di adrenalina.</p>
